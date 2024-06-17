0ed28b15b827de23d9248626e7b61d47da3e414c
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-17 16:33:33 -0700

    new order panel

--- app/(trade)/new-order-panel.tsx
+++ app/(trade)/new-order-panel.tsx
@@ -2,18 +2,28 @@ import { Button } from '@/components/ui/button'
 
 export function NewOrderPanel() {
   return (
-    <aside className="min-w-full p-4">
-      <div className="flex flex-col space-y-4">
+    <aside className="flex min-h-full flex-col justify-between">
+      <div className="flex flex-col">
         <div className="flex items-center justify-between">
-          <Button variant="outline" className="flex-1">
+          <Button
+            variant="outline"
+            className="flex-1 font-serif text-3xl font-bold"
+            size="xl"
+          >
             BUY
           </Button>
-          <Button variant="outline" className="flex-1">
+          <Button
+            variant="outline"
+            className="flex-1 font-serif text-3xl font-bold"
+            size="xl"
+          >
             WETH
           </Button>
         </div>
-        <div className="text-4xl font-bold">4000 USDC</div>
-        <div className="flex space-x-2">
+        <div className="px-6 py-6 text-right font-mono text-4xl">
+          4000&nbsp;<span className="font-serif font-bold">USDC</span>
+        </div>
+        <div className="flex border-b border-input px-6 pb-6">
           <Button variant="outline" className="flex-1">
             25%
           </Button>
@@ -24,7 +34,7 @@ export function NewOrderPanel() {
             MAX
           </Button>
         </div>
-        <div className="space-y-2">
+        <div className="space-y-6 p-6">
           <div className="flex justify-between">
             <span>Est. fees for your order</span>
             <span>$2.11</span>
@@ -38,19 +48,30 @@ export function NewOrderPanel() {
             <span>$10.87</span>
           </div>
         </div>
-        <Button className="w-full">Sell WBTC</Button>
-        <p className="text-xs text-gray-500">
-          All orders are pre-trade and post-trade private. Learn more
-        </p>
-        <div className="space-y-2">
-          <div className="flex justify-between">
-            <span>WETH</span>
-            <span>1,785</span>
+        <div className="space-y-2 px-6">
+          <Button
+            variant="outline"
+            className="w-full font-extended text-3xl"
+            size="xl"
+          >
+            Sell WBTC
+          </Button>
+          <div className="space-y-0.5">
+            <p className="text-xs text-gray-500">
+              All orders are pre-trade and post-trade private.
+            </p>
+            <p className="text-xs text-gray-500">Learn more</p>
           </div>
-          <div className="flex justify-between">
-            <span>USDC</span>
-            <span>176,911.00</span>
-          </div>
+        </div>
+      </div>
+      <div className="space-y-2 border-t border-input p-6">
+        <div className="flex justify-between">
+          <span>WETH</span>
+          <span>1,785</span>
+        </div>
+        <div className="flex justify-between">
+          <span>USDC</span>
+          <span>176,911.00</span>
         </div>
       </div>
     </aside>
