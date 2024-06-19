0f32b0f955c228f44af7d11635bcf30bed60a6c2
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-19 14:33:23 -0700

    scroll behavior what we want

--- app/(trade)/page.tsx
+++ app/(trade)/page.tsx
@@ -1,9 +1,8 @@
+import { PageClient } from '@/app/(trade)/page-client'
 import { TokensMarquee } from '@/app/(trade)/tokens-marquee'
-import { cookies } from 'next/headers'
-
 import { Footer } from '@/app/footer'
 import { Header } from '@/app/header'
-import { PageClient } from '@/app/(trade)/page-client'
+import { cookies } from 'next/headers'
 
 export default function Page() {
   const layout = cookies().get('react-resizable-panels:layout')
@@ -16,7 +15,7 @@ export default function Page() {
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex-1">
         <PageClient defaultLayout={defaultLayout} />
       </div>
       <TokensMarquee />
