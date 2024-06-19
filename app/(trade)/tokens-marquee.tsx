362c6c386f86b4ac2b10aa5e9b1fb67ebe81dd6a
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-18 19:51:32 -0700

    prettier

--- app/(trade)/tokens-marquee.tsx
+++ app/(trade)/tokens-marquee.tsx
@@ -3,7 +3,7 @@ import { Fragment } from 'react'
 export function TokensMarquee() {
   const arr = Array.from({ length: 10 })
   return (
-    <div className="h-marquee flex w-full items-center justify-evenly border-y border-input font-extended text-sm">
+    <div className="flex h-marquee w-full items-center justify-evenly border-y border-input font-extended text-sm">
       {arr.map((_, index) => (
         <Fragment key={index}>
           <div className="space-x-2">
