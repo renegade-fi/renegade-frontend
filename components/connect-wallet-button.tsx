6789fb7de2869d5b2a9e4abd5f8bc0f3198e58d8
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-19 11:04:24 -0700

    connect wallet works with 0 flash

--- components/connect-wallet-button.tsx
+++ components/connect-wallet-button.tsx
@@ -1,21 +1,27 @@
 import { Button } from '@/components/ui/button'
-import { ConnectKitButton } from 'connectkit'
+import { useModal } from 'connectkit'
+import { useAccount, useDisconnect } from 'wagmi'
 
 export function ConnectWalletButton() {
+  const { status, address } = useAccount()
+  const { setOpen } = useModal()
+  const { disconnect } = useDisconnect()
+
+  const handleClick = () => {
+    if (status === 'connected' || status === 'reconnecting') {
+      disconnect()
+    } else {
+      setOpen(true)
+    }
+  }
+
   return (
-    // <ConnectKitButton.Custom>
-    //   {({ isConnected, isConnecting, show, hide, address, ensName, chain }) => {
-    //     return (
-    //       <Button
-    //         onClick={show}
-    //         variant="shimmer"
-    //         className="font-extended text-base"
-    //       >
-    //         {isConnected ? chain?.name : 'Connect Wallet'}
-    //       </Button>
-    //     )
-    //   }}
-    // </ConnectKitButton.Custom>
-    <ConnectKitButton />
+    <Button
+      onClick={handleClick}
+      variant="shimmer"
+      className="font-extended text-base"
+    >
+      {address ? `Disconnect ${address?.slice(0, 6)}` : 'Connect Wallet'}
+    </Button>
   )
 }
