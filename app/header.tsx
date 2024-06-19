7e9ec630c8ce39f424ed99c02eca4323f5317c05
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-19 14:59:31 -0700

    header footer good on mobile

--- app/header.tsx
+++ app/header.tsx
@@ -6,35 +6,37 @@ import Image from 'next/image'
 
 export function Header() {
   return (
-    <header className="grid min-h-20 grid-cols-3 items-center">
-      <div className="pl-6">
-        <Image
-          src="/glyph_dark.svg"
-          alt="logo"
-          width="31"
-          height="38"
-          priority
-        />
-      </div>
-      <nav className="flex space-x-5 justify-self-center font-extended">
-        <a href="#" className="hover:underline">
-          Trade
-        </a>
-        <a href="#" className="text-muted-foreground hover:underline">
-          Assets
-        </a>
-        <a href="#" className="text-muted-foreground hover:underline">
-          Orders
-        </a>
-        <a href="#" className="text-muted-foreground hover:underline">
-          Stats
-        </a>
-      </nav>
-      <div className="flex items-center space-x-4 justify-self-end pr-4">
-        <Button className="font-extended" variant="outline">
-          Deposit
-        </Button>
-        <ConnectWalletButton />
+    <header className="fixed top-0 z-10 min-w-full bg-background">
+      <div className="grid min-h-20 grid-cols-3 items-center">
+        <div className="pl-6">
+          <Image
+            src="/glyph_dark.svg"
+            alt="logo"
+            width="31"
+            height="38"
+            priority
+          />
+        </div>
+        <nav className="flex space-x-5 justify-self-center font-extended">
+          <a href="#" className="hover:underline">
+            Trade
+          </a>
+          <a href="#" className="text-muted-foreground hover:underline">
+            Assets
+          </a>
+          <a href="#" className="text-muted-foreground hover:underline">
+            Orders
+          </a>
+          <a href="#" className="text-muted-foreground hover:underline">
+            Stats
+          </a>
+        </nav>
+        <div className="flex items-center space-x-4 justify-self-end pr-4">
+          <Button className="font-extended" variant="outline">
+            Deposit
+          </Button>
+          <ConnectWalletButton />
+        </div>
       </div>
     </header>
   )
