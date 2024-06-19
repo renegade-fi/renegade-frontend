1320ed210988bcd7f5d9d7e36eac1590fc858707
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-18 19:38:02 -0700

    marquee spacing

--- app/(trade)/bbo-marquee.tsx
+++ app/(trade)/bbo-marquee.tsx
@@ -1,23 +1,31 @@
 export function BBOMarquee() {
   return (
-    <div className="h-marquee flex w-full items-center justify-evenly border-y border-input font-extended">
+    <div className="h-marquee flex w-full items-center justify-evenly border-y border-input font-extended text-sm">
       <span>BBO Feeds</span>
       <span className="text-xs">•</span>
-      <span>Binance</span>
-      <span>$3,756.89</span>
-      <span className="font-extended text-green-price">LIVE</span>
+      <div className="space-x-6">
+        <span>Binance</span>
+        <span>$3,756.89</span>
+        <span className="font-extended text-green-price">LIVE</span>
+      </div>
       <span className="text-xs">•</span>
-      <span>Coinbase</span>
-      <span>$3,756.89</span>
-      <span className="font-extended text-green-price">LIVE</span>
+      <div className="space-x-6">
+        <span>Coinbase</span>
+        <span>$3,756.89</span>
+        <span className="font-extended text-green-price">LIVE</span>
+      </div>
       <span className="text-xs">•</span>
-      <span>Kraken</span>
-      <span>$3,756.89</span>
-      <span className="font-extended text-green-price">LIVE</span>
+      <div className="space-x-6">
+        <span>Kraken</span>
+        <span>$3,756.89</span>
+        <span className="font-extended text-green-price">LIVE</span>
+      </div>
       <span className="text-xs">•</span>
-      <span>Okx</span>
-      <span>$3,756.89</span>
-      <span className="font-extended text-green-price">LIVE</span>
+      <div className="space-x-6">
+        <span>Okx</span>
+        <span>$3,756.89</span>
+        <span className="font-extended text-green-price">LIVE</span>
+      </div>
     </div>
   )
 }
