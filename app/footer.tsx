2728125a0d1e0b030a7b141ee38d9a3febbf5fa2
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-18 19:51:23 -0700

    footer done

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,24 +1,30 @@
 'use client'
 
-import Image from 'next/image'
 import {
   DiscordLogoIcon,
   GitHubLogoIcon,
   TwitterLogoIcon,
 } from '@radix-ui/react-icons'
 import { Book } from 'lucide-react'
+import Image from 'next/image'
 
 export function Footer() {
   return (
-    <footer className="flex items-center p-4">
-      <Image src="/logo_dark.svg" alt="logo" width="192" height="30" priority />
-      <div className="flex justify-between">
-        <div className="flex space-x-4">
-          <TwitterLogoIcon className="h-6 w-6" />
-          <DiscordLogoIcon className="h-6 w-6" />
-          <GitHubLogoIcon className="h-6 w-6" />
-          <Book className="h-6 w-6" />
-        </div>
+    <footer className="grid h-20 grid-cols-2 items-center">
+      <div className="pl-6">
+        <Image
+          src="/logo_dark.svg"
+          alt="logo"
+          width="192"
+          height="30"
+          priority
+        />
+      </div>
+      <div className="flex space-x-4 justify-self-end pr-6">
+        <TwitterLogoIcon className="h-6 w-6" />
+        <DiscordLogoIcon className="h-6 w-6" />
+        <GitHubLogoIcon className="h-6 w-6" />
+        <Book className="h-6 w-6" />
       </div>
     </footer>
   )
