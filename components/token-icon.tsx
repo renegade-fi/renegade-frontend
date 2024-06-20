1c2084e3fe3ff531eea89158b35a25cf176f15ba
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-19 18:10:50 -0700

    assets section

---
+++ components/token-icon.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export function TokenIcon({
+  ticker,
+  size = 32,
+}: {
+  ticker: string
+  size?: number
+}) {
+  return (
+    <Image
+      src={`/tokens/${ticker.toLowerCase()}.png`}
+      alt={ticker}
+      width={size}
+      height={size}
+    />
+  )
+}
