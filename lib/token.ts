c5214a51a06ea65a76d38f4fe488bc67da98d9c9
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-20 14:38:06 -0700

    token select in deposit dialog working

---
+++ lib/token.ts
@@ -0,0 +1,25 @@
+import { tokenMapping } from '@renegade-fi/react/constants'
+
+export const HIDDEN_TICKERS = ['USDT', 'REN']
+export const STABLECOINS = ['USDC', 'USDT']
+
+export const DISPLAY_TOKENS = (
+  options: {
+    hideStables?: boolean
+    hideHidden?: boolean
+    hideTickers?: Array<string>
+  } = {},
+) => {
+  const { hideStables, hideHidden = true, hideTickers = [] } = options
+  let tokens = tokenMapping.tokens
+  if (hideStables) {
+    tokens = tokens.filter(token => !STABLECOINS.includes(token.ticker))
+  }
+  if (hideHidden) {
+    tokens = tokens.filter(token => !HIDDEN_TICKERS.includes(token.ticker))
+  }
+  if (hideTickers.length > 0) {
+    tokens = tokens.filter(token => !hideTickers.includes(token.ticker))
+  }
+  return tokens
+}
