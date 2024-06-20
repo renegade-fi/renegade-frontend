cdc9a1eee1a1f24635ef97e36f0cf980a8ab8572
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-20 16:31:38 -0700

    fix glow

--- components/glow-text.tsx
+++ components/glow-text.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
-
 export interface GlowTextProps {
   text: string
   className?: string
 }
 
 export function GlowText({ text, className = '' }: GlowTextProps) {
-  const blurLevels = ['xs', 'md', 'xl']
+  // const blurLevels = ['xs', 'md', 'xl']
 
   return (
     <div>
-      {blurLevels.map((level, index) => (
-        <span
-          key={index}
-          className={cn(
-            'absolute select-none bg-clip-text font-extrabold text-transparent',
-            className,
-            `blur-${level}`,
-          )}
-        >
-          {text}
-        </span>
-      ))}
-      <span className={cn(className, 'bg-clip-text text-transparent')}>
+      <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-sm">
         {text}
       </span>
+      <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-md">
+        {text}
+      </span>
+      {/* <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-xl">
+        {text}
+      </span> */}
+      <h1 className="relative top-0 flex h-auto w-fit select-auto items-center justify-center bg-green-price bg-clip-text font-extrabold text-transparent">
+        {text}
+      </h1>
     </div>
+    // <div>
+    //   {blurLevels.map((level, index) => (
+    //     <span
+    //       key={index}
+    //       className={cn(
+    //         'absolute select-none bg-clip-text font-extrabold text-transparent',
+    //         className,
+    //         `blur-${level}`,
+    //       )}
+    //     >
+    //       {text}
+    //     </span>
+    //   ))}
+    //   <span className={cn(className, 'bg-clip-text text-transparent')}>
+    //     {text}
+    //   </span>
+    // </div>
   )
 }
