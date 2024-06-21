2cec59c0e89f79b825e4c3fa0cfb7f4e5226a024
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-20 23:18:14 -0700

    new order panel touch up

--- app/trade/[base]/side-button.tsx
+++ app/trade/[base]/side-button.tsx
@@ -1,15 +1,17 @@
 import { setSide } from '@/app/trade/[base]/actions'
 import { Button } from '@/components/ui/button'
+import { ArrowRightLeft } from 'lucide-react'
 
 export function SideButton({ side }: { side: string }) {
   return (
     <Button
       variant="outline"
-      className="flex-1 border-l-0 font-serif text-3xl font-bold"
+      className="flex-1 border-l-0 font-serif text-2xl font-bold"
       size="xl"
       onClick={() => setSide(side === 'buy' ? 'sell' : 'buy')}
     >
       {side.toUpperCase()}
+      <ArrowRightLeft className="ml-2 h-4 w-4" />
     </Button>
   )
 }
