7e323463c0057160c1d7a94fdd025afa76c29e11
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-20 21:26:41 -0700

    token marquee working

--- app/trade/[base]/tokens-marquee.tsx
+++ app/trade/[base]/tokens-marquee.tsx
@@ -1,18 +1,37 @@
+import { DISPLAY_TOKENS } from '@/lib/token'
+import Link from 'next/link'
 import { Fragment } from 'react'
 
 export function TokensMarquee() {
   const arr = Array.from({ length: 10 })
   return (
-    <div className="flex min-h-marquee items-center justify-evenly overflow-x-hidden whitespace-nowrap border-y border-input font-extended text-sm">
-      {arr.map((_, index) => (
-        <Fragment key={index}>
-          <div className="space-x-2">
-            <span>WBTC</span>
-            <span>$71,456.12</span>
-          </div>
-          <span className="text-xs">•</span>
-        </Fragment>
-      ))}
+    <div className="group relative ml-8 flex min-h-marquee items-center overflow-hidden border-y font-extended text-sm">
+      <div className="animate-marquee space-x-8 whitespace-nowrap group-hover:paused">
+        {DISPLAY_TOKENS().map((token, index) => (
+          <Fragment key={index}>
+            <Link href={`/trade/${token.ticker}`}>
+              <span className="space-x-6">
+                <span>{token.ticker}</span>
+                <span>$71,456.12</span>
+              </span>
+            </Link>
+            <span className="text-xs">•</span>
+          </Fragment>
+        ))}
+      </div>
+      <div className="animate-marquee2 absolute ml-8 space-x-8 whitespace-nowrap group-hover:paused">
+        {DISPLAY_TOKENS().map((token, index) => (
+          <Fragment key={index}>
+            <Link href={`/trade/${token.ticker}`}>
+              <span className="space-x-6">
+                <span>{token.ticker}</span>
+                <span>$71,456.12</span>
+              </span>
+            </Link>
+            <span className="text-xs">•</span>
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }
