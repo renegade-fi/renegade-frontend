82069f09c4246a904b4b3c7034327ddb60bc4f34
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-21 15:54:49 -0700

    renegade connect works with cookies

--- components/renegade-provider/config.ts
+++ components/renegade-provider/config.ts
@@ -1,16 +1,17 @@
-'use client'
-
 import { viemClient } from '@/lib/viem'
-import { RenegadeProvider as Provider, createConfig } from '@renegade-fi/react'
-
+import {
+  RenegadeProvider as Provider,
+  cookieStorage,
+  createConfig,
+  createStorage,
+} from '@renegade-fi/react'
 export const config = createConfig({
   darkPoolAddress: process.env.NEXT_PUBLIC_DARKPOOL_CONTRACT,
   priceReporterUrl: process.env.NEXT_PUBLIC_PRICE_REPORTER_URL,
   relayerUrl: process.env.NEXT_PUBLIC_RENEGADE_RELAYER_HOSTNAME,
   ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
   viemClient,
 })
-
-export function RenegadeProvider({ children }: { children: React.ReactNode }) {
-  return <Provider config={config}>{children}</Provider>
-}
--- components/wagmi-provider/wagmi-provider.tsx
+++ components/wagmi-provider/wagmi-provider.tsx
@@ -3,9 +3,16 @@
 import { SignInDialog } from '@/components/dialogs/sign-in-dialog'
 import { QueryProvider } from '@/components/query-provider'
 import { ConnectKitProvider } from 'connectkit'
-import { useState } from 'react'
-import { WagmiProvider as Provider, State } from 'wagmi'
+import { useEffect, useState } from 'react'
+import {
+  WagmiProvider as Provider,
+  State,
+  useAccount,
+  useAccountEffect,
+} from 'wagmi'
 import { config } from './config'
+import { useConfig } from '@renegade-fi/react'
+import { disconnect } from '@renegade-fi/react/actions'
 
 export function WagmiProvider({
   initialState,
@@ -21,9 +28,54 @@ export function WagmiProvider({
       <QueryProvider>
         <ConnectKitProvider onConnect={onOpenChange}>
           {children}
+          <SyncRenegadeWagmiState />
         </ConnectKitProvider>
         <SignInDialog open={open} onOpenChange={onOpenChange} />
       </QueryProvider>
     </Provider>
   )
+}
+
+function SyncRenegadeWagmiState() {
+  const config = useConfig()
+  const { address, connector } = useAccount()
+
+  // Disconnect on wallet change
+  useEffect(() => {
+    const handleConnectorUpdate = (
+      data: {
+        accounts?: readonly `0x${string}`[] | undefined
+        chainId?: number | undefined
+      } & {
+        uid: string
+      },
+    ) => {
+      if (data.accounts) {
+        disconnect(config)
+      }
+    }
+
+    if (connector?.emitter) {
+      connector.emitter.on('change', handleConnectorUpdate)
+    }
+
+    return () => {
+      if (connector?.emitter) {
+        connector?.emitter.off('change', handleConnectorUpdate)
+      }
+    }
+  }, [config, connector])
+
+  useAccountEffect({
+    onDisconnect() {
+      disconnect(config)
+    },
+  })
+
+  useEffect(() => {
+    if (!address) {
+      disconnect(config)
+    }
+  }, [address, config])
+  return <></>
 }
