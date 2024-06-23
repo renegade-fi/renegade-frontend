234581563fd4d2eb8b2d229f33166de2116b5ffd
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-22 17:35:06 -0700

    spacing on assets page

--- app/assets/page-client.tsx
+++ app/assets/page-client.tsx
@@ -1,14 +1,16 @@
 import { AssetsTable } from '@/app/assets/assets-table'
 import { TransferHistoryTable } from '@/app/assets/transfer-history-table'
+import { Separator } from '@/components/ui/separator'
 
 export function PageClient() {
   return (
     <main>
-      <div className="container space-y-6">
+      <div className="container space-y-12">
         <div>
           <h1 className="mt-6 font-serif text-3xl font-bold">Assets</h1>
           <AssetsTable />
         </div>
+        <Separator />
         <div>
           <h1 className="mt-6 font-serif text-3xl font-bold">
             Transfer History
