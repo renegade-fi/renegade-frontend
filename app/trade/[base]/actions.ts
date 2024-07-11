807d4ed32e2c5d58b7f6c3bc560e9f98193d0f88
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-11 15:15:55 -0700

    change name

--- app/trade/[base]/actions.ts
+++ app/trade/[base]/actions.ts
@@ -2,12 +2,12 @@
 
 import { cookies } from 'next/headers'
 
-import { STORAGE_SIDE, STORAGE_USE_USDC } from '@/lib/constants/storage'
+import { STORAGE_SIDE, STORAGE_USDC_DENOMINATED } from '@/lib/constants/storage'
 
 export async function setSide(side: 'buy' | 'sell') {
   cookies().set(STORAGE_SIDE, side)
 }
 
-export async function setUseUSDC(useUSDC: boolean) {
-  cookies().set(STORAGE_USE_USDC, useUSDC ? 'true' : 'false')
+export async function setUseUSDC(isUSDCDenominated: boolean) {
+  cookies().set(STORAGE_USDC_DENOMINATED, isUSDCDenominated ? 'true' : 'false')
 }
--- lib/constants/storage.ts
+++ lib/constants/storage.ts
@@ -4,4 +4,4 @@ export const STORAGE_SIDE = `${STORAGE_PREFIX}.side`
 export const STORAGE_LAYOUT = `${STORAGE_PREFIX}.layout`
 
 export const STORAGE_AMOUNT = `${STORAGE_PREFIX}.order.amount`
-export const STORAGE_USE_USDC = `${STORAGE_PREFIX}.order.useUSDC`
+export const STORAGE_USDC_DENOMINATED = `${STORAGE_PREFIX}.order.isUSDCDenominated`
