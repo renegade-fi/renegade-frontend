0c6e0fd43b92399ba115faaa585093f3e05cfb2e
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-11 14:58:07 -0700

    add fee section

--- components/glow-text.tsx
+++ components/glow-text.tsx
@@ -1,23 +1,53 @@
+import { cn } from '@/lib/utils'
+
 export interface GlowTextProps {
+  enabled?: boolean
   text: string
   className?: string
 }
 
-export function GlowText({ text, className = '' }: GlowTextProps) {
+export function GlowText({ className, enabled = true, text }: GlowTextProps) {
   // const blurLevels = ['xs', 'md', 'xl']
 
   return (
     <div>
-      <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-sm">
+      <span
+        className={cn(
+          'absolute mx-auto box-content flex w-fit select-none border  bg-clip-text font-extrabold text-transparent blur-sm',
+          className,
+          enabled ? 'blur-sm' : 'blur-0',
+          {
+            hidden: !enabled,
+          },
+        )}
+      >
         {text}
       </span>
-      <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-md">
+      <span
+        className={cn(
+          'absolute mx-auto box-content flex w-fit select-none border  bg-clip-text font-extrabold text-transparent blur-md',
+          className,
+          enabled ? 'blur-md' : 'blur-0',
+          {
+            hidden: !enabled,
+          },
+        )}
+      >
         {text}
       </span>
       {/* <span className="absolute mx-auto box-content flex w-fit select-none border bg-green-price bg-clip-text font-extrabold text-transparent blur-xl">
         {text}
       </span> */}
-      <h1 className="relative top-0 flex h-auto w-fit select-auto items-center justify-center bg-green-price bg-clip-text font-extrabold text-transparent">
+      <h1
+        className={cn(
+          'relative top-0 flex h-auto w-fit select-auto items-center justify-center  bg-clip-text  text-transparent',
+          enabled ? className : '',
+          {
+            'text-inherit': !enabled,
+            'font-extrabold': enabled,
+          },
+        )}
+      >
         {text}
       </h1>
     </div>
--- lib/constants/protocol.ts
+++ lib/constants/protocol.ts
@@ -1,2 +1,7 @@
 // Min fill size of the quote asset that the relayer will accept
 export const MIN_FILL_SIZE = BigInt(1000000000000000000)
+// TODO: [CORRECTNESS] Should fetch from relayer
+// Relayer fee
+export const RELAYER_FEE = 0.008
+// Protocol fee
+export const PROTOCOL_FEE = 0.002
