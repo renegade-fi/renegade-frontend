d5d77998a2e3a3d83a637be950b43152516565eb
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-13 13:09:51 -0700

    show assets in USD, with tooltip

--- app/trade/[base]/assets-section.tsx
+++ app/trade/[base]/assets-section.tsx
@@ -1,8 +1,15 @@
 import { Token, useBalances } from '@renegade-fi/react'
 
 import { TokenIcon } from '@/components/token-icon'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
-import { formatNumber } from '@/lib/format'
+import { useUSDPrice } from '@/hooks/use-usd-price'
+import { formatCurrency, formatNumber } from '@/lib/format'
 
 export function AssetsSection({
   base,
@@ -11,40 +18,59 @@ export function AssetsSection({
   base: string
   quote?: string
 }) {
-  const baseToken = Token.findByTicker(base)
-  const quoteToken = Token.findByTicker(quote)
-
   const balances = useBalances()
-  const baseBalance = balances.get(baseToken.address)?.amount
-  const quoteBalance = balances.get(quoteToken.address)?.amount
 
+  const baseToken = Token.findByTicker(base)
+  const baseBalance = balances.get(baseToken.address)?.amount
   const formattedBaseBalance = formatNumber(
     baseBalance ?? BigInt(0),
     baseToken.decimals,
   )
+  const baseUsdPrice = useUSDPrice(baseToken, baseBalance)
+
+  const quoteToken = Token.findByTicker(quote)
+  const quoteBalance = balances.get(quoteToken.address)?.amount
   const formattedQuoteBalance = formatNumber(
     quoteBalance ?? BigInt(0),
     quoteToken.decimals,
   )
+  const quoteUsdPrice = useUSDPrice(quoteToken, quoteBalance)
+
   return (
-    <div className="p-6">
-      <h2 className="mb-4">Your Assets</h2>
-      <div className="space-y-2">
-        <div className="flex justify-between">
-          <div className="flex items-center space-x-2">
-            <TokenIcon ticker={base} size={20} />
-            <span>{base}</span>
+    <TooltipProvider>
+      <div className="p-6">
+        <h2 className="mb-4">Your Assets</h2>
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <div className="flex items-center space-x-2">
+              <TokenIcon ticker={base} size={20} />
+              <span>{base}</span>
+            </div>
+            <Tooltip>
+              <TooltipTrigger>
+                <span>{formatCurrency(baseUsdPrice)}</span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{`${formattedBaseBalance} ${base}`}</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
-          <span>{formattedBaseBalance}</span>
-        </div>
-        <div className="flex justify-between">
-          <div className="flex items-center space-x-2">
-            <TokenIcon ticker={quote} size={20} />
-            <span>{quote}</span>
+          <div className="flex justify-between">
+            <div className="flex items-center space-x-2">
+              <TokenIcon ticker={quote} size={20} />
+              <span>{quote}</span>
+            </div>
+            <Tooltip>
+              <TooltipTrigger>
+                <span>{formatCurrency(quoteUsdPrice)}</span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{`${formattedQuoteBalance} ${quote}`}</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
-          <span>{formattedQuoteBalance}</span>
         </div>
       </div>
-    </div>
+    </TooltipProvider>
   )
 }
