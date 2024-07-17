e2363772bc21ebb2f8be2799d62c7d52ef59af94
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-17 15:10:38 -0700

    hide savings column on trade page

--- app/trade/[base]/page-client.tsx
+++ app/trade/[base]/page-client.tsx
@@ -88,6 +88,7 @@ export function PageClient({
                   initialVisibleColumns={{
                     "time to fill": false,
                     actions: false,
+                    saved: false,
                   }}
                   isTradePage
                 />
