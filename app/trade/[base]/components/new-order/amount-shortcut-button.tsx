3c586ca5fd469bd076621a208624e300ec286eae
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-19 11:09:52 -0700

    shortcut buttons work

--- app/trade/[base]/components/new-order/amount-shortcut-button.tsx
+++ app/trade/[base]/components/new-order/amount-shortcut-button.tsx
@@ -1,10 +1,21 @@
+import React from "react"
+
 import { Token, parseAmount, useBalances } from "@renegade-fi/react"
+
+import { NewOrderFormProps } from "@/app/trade/[base]/components/new-order/new-order-form"
 
 import { Button } from "@/components/ui/button"
 
 import { MIN_FILL_SIZE } from "@/lib/constants/protocol"
 import { formatNumber } from "@/lib/format"
 import { cn } from "@/lib/utils"
+import { usePrice } from "@/stores/price-store"
+
+interface AmountShortcutButtonProps extends NewOrderFormProps {
+  className?: string
+  onSetAmount: (amount: number) => void
+  percentage: number
+}
 
 // Percentage should be <= 1
 export function AmountShortcutButton({
@@ -12,20 +23,49 @@ export function AmountShortcutButton({
   className,
   onSetAmount,
   percentage,
-}: {
-  base: string
-  className?: string
-  onSetAmount: (amount: number) => void
-  percentage: number
-}) {
+  isSell,
+  isUSDCDenominated,
+}: AmountShortcutButtonProps) {
   const data = useBalances()
   const token = Token.findByTicker(base)
-  if (!token) return null
+  const quoteToken = Token.findByTicker("USDC")
+  const price = usePrice({
+    baseAddress: token.address,
+  })
 
-  const balance = data.get(token.address)?.amount ?? BigInt(0)
-  const formattedBalance = Number(formatNumber(balance, token.decimals))
-  const shortcut = formattedBalance * percentage
-  const isDisabled = balance === BigInt(0)
+  const maxBalance = React.useMemo(() => {
+    const baseBalance = data.get(token.address)?.amount ?? BigInt(0)
+    const quoteBalance = data.get(quoteToken.address)?.amount ?? BigInt(0)
+    const formattedBaseBalance = Number(
+      formatNumber(baseBalance, token.decimals),
+    )
+    const formattedQuoteBalance = Number(
+      formatNumber(quoteBalance, quoteToken.decimals),
+    )
+    if (!price) return 0
+    if (isSell) {
+      if (isUSDCDenominated) {
+        return formattedBaseBalance * price
+      }
+      return formattedBaseBalance
+    }
+    if (isUSDCDenominated) {
+      return formattedQuoteBalance
+    }
+    return formattedQuoteBalance / price
+  }, [
+    data,
+    isSell,
+    isUSDCDenominated,
+    price,
+    quoteToken.address,
+    quoteToken.decimals,
+    token.address,
+    token.decimals,
+  ])
+
+  const shortcut = maxBalance * percentage
+  const isDisabled = maxBalance === 0
 
   // TODO: [SAFETY] Calculate the minimum fill size of the quote asset and ensure it is greater than the minimum fill size
   // parseAmount(shortcut.toString(), token) < MIN_FILL_SIZE
