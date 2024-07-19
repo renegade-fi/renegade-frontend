fd3c85a38eb8128e3350052fd834f18e770df5ea
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-19 11:09:52 -0700

    shortcut buttons work

--- app/trade/[base]/components/new-order/new-order-form.tsx
+++ app/trade/[base]/components/new-order/new-order-form.tsx
@@ -62,8 +62,8 @@ export function NewOrderForm({
       isUSDCDenominated: isUSDCDenominated ?? false,
     },
   })
-
   const fees = usePredictedFees(form.watch())
+  const [open, setOpen] = React.useState(false)
 
   React.useEffect(() => {
     const subscription = form.watch((value, { name, type }) => {
@@ -78,8 +78,6 @@ export function NewOrderForm({
     return () => subscription.unsubscribe()
   }, [form])
 
-  const [open, setOpen] = React.useState(false)
-
   function onSubmit(values: z.infer<typeof formSchema>) {
     form.trigger().then(isValid => {
       if (isValid) {
@@ -90,19 +88,75 @@ export function NewOrderForm({
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <div className="flex flex-col">
-          <div className="flex items-center justify-between">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="">
+        <div className="flex">
+          <FormField
+            control={form.control}
+            name="isSell"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <Button
+                    variant="outline"
+                    className="w-full border-l-0 font-serif text-2xl font-bold"
+                    size="xl"
+                    {...field}
+                    value={""}
+                    onClick={e => {
+                      e.preventDefault()
+                      field.onChange(!field.value)
+                    }}
+                  >
+                    {field.value ? "SELL" : "BUY"}
+                    <ArrowRightLeft className="ml-2 h-5 w-5" />
+                  </Button>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <TokenSelectDialog>
+            <Button
+              variant="outline"
+              className="flex-1 border-x-0 font-serif text-2xl font-bold"
+              size="xl"
+            >
+              {base}
+              <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </TokenSelectDialog>
+        </div>
+        <div className="p-6">
+          <Label className="font-sans text-muted-foreground">Amount</Label>
+          <div className="flex items-baseline">
             <FormField
               control={form.control}
-              name="isSell"
+              name="amount"
               render={({ field }) => (
                 <FormItem className="flex-1">
                   <FormControl>
+                    <Input
+                      className="rounded-none border-none px-0 text-right font-mono text-2xl [appearance:textfield] placeholder:text-right focus-visible:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                      placeholder="0.00"
+                      {...field}
+                      value={field.value === 0 ? "" : field.value}
+                      type="number"
+                      onWheel={e => (e.target as HTMLElement).blur()}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isUSDCDenominated"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
                     <Button
-                      variant="outline"
-                      className="w-full border-l-0 font-serif text-2xl font-bold"
-                      size="xl"
+                      variant="ghost"
+                      className="h-12 flex-1 rounded-none px-2 py-0 font-serif text-2xl font-bold"
                       {...field}
                       value={""}
                       onClick={e => {
@@ -110,7 +164,7 @@ export function NewOrderForm({
                         field.onChange(!field.value)
                       }}
                     >
-                      {field.value ? "SELL" : "BUY"}
+                      {field.value ? "USDC" : base}
                       <ArrowRightLeft className="ml-2 h-5 w-5" />
                     </Button>
                   </FormControl>
@@ -118,97 +172,39 @@ export function NewOrderForm({
                 </FormItem>
               )}
             />
-            <TokenSelectDialog>
-              <Button
-                variant="outline"
-                className="flex-1 border-x-0 font-serif text-2xl font-bold"
-                size="xl"
-              >
-                {base}
-                <ChevronDown className="ml-2 h-4 w-4" />
-              </Button>
-            </TokenSelectDialog>
           </div>
-          <div className="p-6">
-            <Label className="font-sans text-muted-foreground">Amount</Label>
-            <div className="flex items-baseline">
-              <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                  <FormItem className="flex-1">
-                    <FormControl>
-                      <Input
-                        className="rounded-none border-none px-0 text-right font-mono text-2xl [appearance:textfield] placeholder:text-right focus-visible:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                        placeholder="0.00"
-                        {...field}
-                        value={field.value === 0 ? "" : field.value}
-                        type="number"
-                        onWheel={e => (e.target as HTMLElement).blur()}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="isUSDCDenominated"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Button
-                        variant="ghost"
-                        className="h-12 flex-1 rounded-none px-2 py-0 font-serif text-2xl font-bold"
-                        {...field}
-                        value={""}
-                        onClick={e => {
-                          e.preventDefault()
-                          field.onChange(!field.value)
-                        }}
-                      >
-                        {field.value ? "USDC" : base}
-                        <ArrowRightLeft className="ml-2 h-5 w-5" />
-                      </Button>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-          <div className="flex px-6 pb-6">
-            <AmountShortcutButton
-              base={isUSDCDenominated ? "USDC" : base}
-              className="flex-1"
-              percentage={0.25}
-              onSetAmount={amount => form.setValue("amount", amount)}
-            />
-            <AmountShortcutButton
-              base={isUSDCDenominated ? "USDC" : base}
-              className="flex-1 border-x-0"
-              percentage={0.5}
-              onSetAmount={amount => form.setValue("amount", amount)}
-            />
-            <AmountShortcutButton
-              base={isUSDCDenominated ? "USDC" : base}
-              className="flex-1"
-              percentage={1}
-              onSetAmount={amount => form.setValue("amount", amount)}
-            />
-          </div>
-          <Separator />
-          <div className="space-y-3 whitespace-nowrap p-6 text-sm text-muted-foreground">
-            <FeesSection {...fees} />
-          </div>
-          <Button
-            variant="outline"
-            className="mx-auto px-6 font-extended text-3xl"
-            size="xl"
-          >
-            {form.getValues("isSell") ? "Sell" : "Buy"} {base}
-          </Button>
         </div>
+        <div className="flex px-6 pb-6">
+          <AmountShortcutButton
+            {...form.watch()}
+            className="flex-1"
+            percentage={0.25}
+            onSetAmount={amount => form.setValue("amount", amount)}
+          />
+          <AmountShortcutButton
+            {...form.watch()}
+            className="flex-1 border-x-0"
+            percentage={0.5}
+            onSetAmount={amount => form.setValue("amount", amount)}
+          />
+          <AmountShortcutButton
+            {...form.watch()}
+            className="flex-1"
+            percentage={1}
+            onSetAmount={amount => form.setValue("amount", amount)}
+          />
+        </div>
+        <Separator />
+        <div className="space-y-3 whitespace-nowrap p-6 text-sm text-muted-foreground">
+          <FeesSection {...fees} />
+        </div>
+        <Button
+          variant="outline"
+          className="mx-auto flex px-6 font-extended text-3xl"
+          size="xl"
+        >
+          {form.getValues("isSell") ? "Sell" : "Buy"} {base}
+        </Button>
         <NewOrderStepper
           {...form.watch()}
           {...fees}
