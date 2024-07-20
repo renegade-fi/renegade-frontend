3abb24366a3f7e0c3af225c132165e1ca28f971a
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-20 15:27:02 -0700

    batch fetch balances in token select dialog

--- components/dialogs/token-select-dialog.tsx
+++ components/dialogs/token-select-dialog.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 import Link from "next/link"
 
+import { useQueryClient } from "@tanstack/react-query"
+import { useAccount, useBlockNumber, useReadContracts } from "wagmi"
+
 import { TokenIcon } from "@/components/token-icon"
 import { Button } from "@/components/ui/button"
 import {
@@ -26,12 +29,40 @@ import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
 
 import { useMediaQuery } from "@/hooks/use-media-query"
+import { formatNumber } from "@/lib/format"
+import { erc20Abi } from "@/lib/generated"
 import { DISPLAY_TOKENS } from "@/lib/token"
-import { cn } from "@/lib/utils"
 
 export function TokenSelectDialog({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = React.useState(false)
   const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  const { address } = useAccount()
+  const { data, queryKey } = useReadContracts({
+    contracts: DISPLAY_TOKENS().map(token => ({
+      address: token.address,
+      abi: erc20Abi,
+      functionName: "balanceOf",
+      args: [address],
+    })),
+    query: {
+      enabled: !!address && open,
+      select: data =>
+        data.map((balance, index) => ({
+          balance: balance.result ?? BigInt(0),
+          token: DISPLAY_TOKENS()[index],
+        })),
+    },
+  })
+
+  const queryClient = useQueryClient()
+  const blockNumber = useBlockNumber({ watch: true })
+
+  React.useEffect(() => {
+    if (blockNumber) {
+      queryClient.invalidateQueries({ queryKey })
+    }
+  }, [blockNumber, queryClient, queryKey])
 
   if (isDesktop) {
     return (
@@ -45,7 +76,7 @@ export function TokenSelectDialog({ children }: { children: React.ReactNode }) {
             </DialogDescription>
           </DialogHeader>
           <ScrollArea className="max-h-[50vh]">
-            <TokenList />
+            <TokenList tokens={data ?? []} />
           </ScrollArea>
         </DialogContent>
       </Dialog>
@@ -63,7 +94,7 @@ export function TokenSelectDialog({ children }: { children: React.ReactNode }) {
           </DrawerDescription>
         </DrawerHeader>
         <ScrollArea className="max-h-[50vh] overflow-auto">
-          <TokenList />
+          <TokenList tokens={data ?? []} />
         </ScrollArea>
         <DrawerFooter className="pt-2">
           <DrawerClose asChild>
@@ -75,10 +106,27 @@ export function TokenSelectDialog({ children }: { children: React.ReactNode }) {
   )
 }
 
-function TokenList({ className }: React.ComponentProps<"form">) {
+function TokenList({
+  tokens,
+}: {
+  tokens: {
+    balance: bigint
+    token: {
+      name: string
+      ticker: string
+      address: `0x${string}`
+      decimals: number
+    }
+  }[]
+}) {
+  const sortedTokens = tokens.sort((a, b) => {
+    if (a.balance === BigInt(0) && b.balance !== BigInt(0)) return 1
+    if (a.balance !== BigInt(0) && b.balance === BigInt(0)) return -1
+    return 0
+  })
   return (
-    <div className={cn("grid items-start", className)}>
-      {DISPLAY_TOKENS().map(token => (
+    <div className="grid items-start">
+      {sortedTokens.map(({ balance, token }) => (
         <Link href={`/trade/${token.ticker}`} key={token.address}>
           <div className="grid grid-cols-[32px_1fr_1fr] items-center gap-4 px-6 py-2 transition-colors hover:bg-accent hover:text-accent-foreground">
             <TokenIcon ticker={token.ticker} />
@@ -86,7 +134,9 @@ function TokenList({ className }: React.ComponentProps<"form">) {
               <p className="text-md font-medium">{token.name}</p>
               <p className="text-xs text-muted-foreground">{token.ticker}</p>
             </div>
-            <div className="justify-self-end font-mono">17123.56</div>
+            <div className="justify-self-end font-mono">
+              {formatNumber(balance, token.decimals, true)}
+            </div>
           </div>
         </Link>
       ))}
