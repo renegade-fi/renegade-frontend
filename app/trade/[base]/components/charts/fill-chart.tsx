a75c4a4469ab27e57d8a8d72593c3e5b6e9b9021
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-24 17:27:58 -0700

    arbitrary resolution fill charts

--- app/trade/[base]/components/charts/fill-chart.tsx
+++ app/trade/[base]/components/charts/fill-chart.tsx
@@ -23,52 +23,41 @@ import {
 
 import { useOHLC } from "@/hooks/use-ohlc"
 import { formatCurrency, formatNumber } from "@/lib/format"
+import { remapToken } from "@/lib/token"
 
 const chartConfig = {
-  renegade: {
+  fillPrice: {
     label: "Renegade Fill",
     color: "hsl(var(--chart-blue))",
   },
-  binance: {
+  price: {
     label: "Binance Price",
     color: "hsl(var(--chart-yellow))",
   },
-  vwap: {
-    label: "VWAP",
-    color: "hsl(var(--chart-blue))",
-  },
-  timestamp: {
-    label: "Date",
-  },
+  // vwap: {
+  //   label: "VWAP",
+  //   color: "hsl(var(--chart-blue))",
+  // },
+  // timestamp: {
+  //   label: "Date",
+  // },
 } satisfies ChartConfig
 
 function calculateYAxisDomain(
   minValue: number,
   maxValue: number,
-  offset: number = 0.0005,
+  offset: number = 0.1,
 ): [number, number] {
-  // Helper function to get the number of digits before the decimal point
-  const getDigits = (num: number): number =>
-    Math.floor(Math.log10(Math.abs(num))) + 1
-
-  // Calculate the order of magnitude for rounding
-  // TODO: Make this dynamic based on the minValue and maxValue
-  const digits = Math.max(getDigits(minValue), getDigits(maxValue)) - 2
-  const roundTo = Math.pow(10, digits - 1)
-
-  // Calculate the domain bounds with a 5% padding
-  const lowerBound =
-    Math.floor((minValue - minValue * offset) / roundTo) * roundTo
-  const upperBound =
-    Math.ceil((maxValue + maxValue * offset) / roundTo) * roundTo
-
+  const padding = (maxValue - minValue) * offset
+  const lowerBound = Math.floor(minValue - padding)
+  const upperBound = Math.ceil(maxValue + padding)
   return [lowerBound, upperBound]
 }
 
 interface ChartData {
-  binance: number
+  price: number
   timestamp: number
-  renegade?: number
+  fillPrice?: number
   vwap?: number
   volume?: number
 }
@@ -84,91 +73,100 @@ export function FillChart({ order }: { order: OrderMetadata }) {
     }))
     .sort((a, b) => a.timestamp - b.timestamp)
 
-  const firstFillTimestamp = formattedFills[0].timestamp
-  const lastFillTimestamp = formattedFills[formattedFills.length - 1].timestamp
+  const resolutionMs = 7 * 1000
 
-  let from: number
-  let to: number
-  const timeSpan =
-    formattedFills.length === 1
-      ? 2 * 60 * 60 * 1000
-      : lastFillTimestamp - firstFillTimestamp
-  const padding = Math.floor(0.1 * timeSpan) // 10% padding on both sides
+  const { newFromMs, newToMs } = React.useMemo(() => {
+    const minFillTimestamp = formattedFills[0].timestamp
+    const maxFillTimestamp = formattedFills[formattedFills.length - 1].timestamp
+    const fillTimeSpan = maxFillTimestamp - minFillTimestamp
 
-  if (formattedFills.length === 1) {
-    const fill = formattedFills[0]
-    from = fill.timestamp - padding
-    to = fill.timestamp + padding
-  } else {
-    from = formattedFills[0].timestamp - padding
-    to = formattedFills[formattedFills.length - 1].timestamp + padding
-  }
-  console.log("debug: ", {
-    from,
-    to,
+    let startTime,
+      endTime = 0
+
+    // Chart width / point spacing
+    const pointsCount = Math.floor(527 / 10)
+    const requiredTimeSpan = pointsCount * resolutionMs
+
+    if (formattedFills.length === 1) {
+      startTime = minFillTimestamp - requiredTimeSpan / 2
+      endTime = maxFillTimestamp + requiredTimeSpan / 2
+    } else {
+      const halfTimeSpan = (requiredTimeSpan - fillTimeSpan) / 2
+      startTime = minFillTimestamp - halfTimeSpan
+      endTime = maxFillTimestamp + halfTimeSpan
+    }
+    return {
+      newFromMs: Math.floor(startTime / 60000) * 60000,
+      newToMs: Math.ceil(endTime / 60000) * 60000,
+    }
+  }, [formattedFills, resolutionMs])
+
+  const { data: ohlc } = useOHLC({
+    instrument: `${remapToken(token.ticker)}_usdt`,
+    startDateMs: newFromMs,
+    endDateMs: newToMs,
+    timeInterval: "minutes",
   })
-  const { data: ohlc } = useOHLC(token.ticker, from, to, "minutes")
+
+  const threshold = 150
 
   const chartData = React.useMemo(() => {
     if (!ohlc) return []
-    const targetElements = 150
-    const resolutionFactor = Math.ceil(ohlc.length / targetElements)
 
-    const temp: Array<ChartData> = ohlc.map(ohlc => ({
-      binance: order.data.side === "Sell" ? ohlc.low : ohlc.high,
-      timestamp: ohlc.time,
-      vwap: undefined,
-    }))
+    const ohlcStartTime = ohlc[0].time
+    const ohlcEndTime = ohlc[ohlc.length - 1].time
 
-    temp.sort((a, b) => a.timestamp - b.timestamp)
+    // Step 1: Convert price data to the desired resolution
+    const adjustedPriceData: Partial<ChartData>[] = []
+    let startTimestamp = Math.floor(newFromMs / resolutionMs) * resolutionMs
+    for (let i = 0; i < ohlc.length; i++) {
+      const bar = ohlc[i]
+      const endTimestamp = bar.time + 60000 // Each bar is 1 minute
+      while (startTimestamp < endTimestamp) {
+        if (startTimestamp >= ohlcStartTime && startTimestamp < ohlcEndTime) {
+          adjustedPriceData.push({
+            timestamp: startTimestamp,
+            price: order.data.side === "Sell" ? bar.low : bar.high,
+          })
+        }
+        startTimestamp += resolutionMs
+      }
+    }
 
-    let cumVolume = 0
-    let cumPriceVolume = 0
-
-    // Map fills to closest candle
-    // Closest candle should not be in the future relative to the fill
-    formattedFills.forEach(fill => {
-      const closest = temp.reduce((prev, curr) => {
-        return curr.timestamp <= fill.timestamp &&
-          curr.timestamp > prev.timestamp
-          ? curr
-          : prev
-      }, temp[0])
-
-      if (closest) {
-        closest.renegade = Number(fill.price.toFixed(2))
-        closest.volume = Number(fill.amount.toFixed(2))
+    const adjustedFillData = formattedFills.map(fillPoint => {
+      // Aligned timestamp will never be greater than fill timestamp
+      // Can be up to resolutionMs off
+      const alignedTimestamp =
+        Math.floor(fillPoint.timestamp / resolutionMs) * resolutionMs
+      return {
+        timestamp: alignedTimestamp,
+        fillPrice: fillPoint.price,
+        volume: fillPoint.amount,
       }
     })
 
-    // Sample data to keep chart performant
-    const sampledTemp = temp.filter(
-      (_, index) => index % resolutionFactor === 0 || temp[index].renegade,
-    )
-
-    // Calculate and set VWAP data within relevant time range
-    sampledTemp.forEach(data => {
-      if (data.renegade && data.volume) {
-        cumVolume += data.volume
-        cumPriceVolume += data.renegade * data.volume
-      }
-      if (
-        data.renegade ||
-        (data.timestamp >= firstFillTimestamp &&
-          data.timestamp <= lastFillTimestamp)
-      ) {
-        data.vwap = cumVolume > 0 ? cumPriceVolume / cumVolume : undefined
+    // Step 3: Merge adjusted price and fill data
+    const result = adjustedPriceData.map(pricePoint => {
+      const fillPoint = adjustedFillData.find(
+        f => f.timestamp === pricePoint.timestamp,
+      ) || { volume: 0, fillPrice: undefined }
+      return {
+        timestamp: pricePoint.timestamp,
+        price: pricePoint.price,
+        fillPrice: fillPoint.fillPrice,
+        volume: fillPoint.volume,
       }
     })
 
-    return sampledTemp
-  }, [
-    firstFillTimestamp,
-    formattedFills,
-    lastFillTimestamp,
-    ohlc,
-    order.data.side,
-  ])
+    // // Step 4: Limit Points (Optional)
+    // if (result.length > threshold) {
+    //   const step = Math.ceil(result.length / threshold)
+    //   const limitedData = result.filter((_, index) => index % step === 0)
+    //   return limitedData
+    // }
+
+    return result
+  }, [formattedFills, newFromMs, ohlc, order.data.side, resolutionMs])
 
   function formatTimestamp(value: number): string {
     const date = new Date(value)
@@ -178,30 +176,27 @@ export function FillChart({ order }: { order: OrderMetadata }) {
     })
   }
 
-  const minValue = React.useMemo(
+  const [minValue, maxValue] = React.useMemo(
     () =>
-      Math.min(
-        ...chartData.map(item =>
+      chartData.reduce(
+        ([min, max], item) => [
           Math.min(
-            item.binance ?? Number.POSITIVE_INFINITY,
-            item.renegade ?? Number.POSITIVE_INFINITY,
+            min,
+            item.price ?? Number.POSITIVE_INFINITY,
+            item.fillPrice ?? Number.POSITIVE_INFINITY,
           ),
-        ),
-      ),
-    [chartData],
-  )
-  const maxValue = React.useMemo(
-    () =>
-      Math.max(
-        ...chartData.map(item =>
           Math.max(
-            item.binance ?? Number.NEGATIVE_INFINITY,
-            item.renegade ?? Number.NEGATIVE_INFINITY,
+            max,
+            item.price ?? Number.NEGATIVE_INFINITY,
+            item.fillPrice ?? Number.NEGATIVE_INFINITY,
           ),
-        ),
+        ],
+        [Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY],
       ),
     [chartData],
   )
+
+  const firstFillIndex = chartData.findIndex(item => item.fillPrice)
 
   return (
     <Card className="border-0">
@@ -230,7 +225,7 @@ export function FillChart({ order }: { order: OrderMetadata }) {
               tickFormatter={formatTimestamp}
             />
             <YAxis
-              dataKey="renegade"
+              dataKey="fillPrice"
               axisLine={false}
               tickLine={false}
               tickCount={5}
@@ -245,54 +240,56 @@ export function FillChart({ order }: { order: OrderMetadata }) {
               }
             />
             <ChartLegend content={<ChartLegendContent />} />
-            <ChartTooltip
-              content={
-                <ChartTooltipContent
-                  labelKey="saving"
-                  className="min-w-[200px]"
-                  hideIndicator
-                  hideLabel
-                  formatter={(value, name, item, index, payload) => {
-                    return (
-                      <div className="flex flex-1 items-center justify-between leading-none">
-                        <div className="grid gap-1.5">
-                          <span className="text-muted-foreground">
-                            {
-                              chartConfig[item.name as keyof typeof chartConfig]
-                                .label
-                            }
-                          </span>
-                        </div>
-                        {item.value && (
-                          <span className="font-mono font-medium tabular-nums text-foreground">
-                            {formatCurrency(Number(item.value))}
-                          </span>
-                        )}
-                      </div>
-                    )
-                  }}
-                />
-              }
-            />
             <Line
-              dataKey="binance"
-              type="natural"
-              fill="var(--color-binance)"
-              stroke="var(--color-binance)"
+              animationEasing="ease-out"
+              animationDuration={750}
+              // isAnimationActive={false}
+              dataKey="price"
+              type="linear"
+              fill="var(--color-price)"
+              stroke="var(--color-price)"
               dot={false}
             />
             <Line
-              dataKey="renegade"
+              isAnimationActive={false}
+              dataKey="fillPrice"
               type="natural"
-              stroke="var(--color-renegade)"
-              fill="var(--color-renegade)"
+              stroke="var(--color-fillPrice)"
+              fill="var(--color-fillPrice)"
             />
-            <Line
+            {/* <Line
               dataKey="vwap"
               type="monotone"
               fill="url(#fillDesktop)"
-              stroke="var(--color-renegade)"
+              stroke="var(--color-fillPrice)"
               dot={false}
+            /> */}
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  hideLabel
+                  className="w-[200px]"
+                  formatter={(value, name, item, index) => (
+                    <>
+                      <div
+                        className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-[--color-bg]"
+                        style={
+                          {
+                            "--color-bg": `var(--color-${name})`,
+                          } as React.CSSProperties
+                        }
+                      />
+                      {chartConfig[name as keyof typeof chartConfig]?.label ||
+                        name}
+                      <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                        {formatCurrency(Number(value))}
+                      </div>
+                    </>
+                  )}
+                />
+              }
+              cursor
+              defaultIndex={firstFillIndex}
             />
           </LineChart>
         </ChartContainer>
