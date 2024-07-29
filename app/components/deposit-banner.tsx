9a10847a67ac5ecf1b823664a7d21d17dd0f500f
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-29 15:52:17 -0700

    display deposit banner if new user

---
+++ app/components/deposit-banner.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useWallet } from "@renegade-fi/react"
+import { ArrowRight } from "lucide-react"
+
+import { TransferDialog } from "@/components/dialogs/transfer-dialog"
+import { Button } from "@/components/ui/button"
+
+export function DepositBanner() {
+  const { data } = useWallet({
+    query: {
+      select: data => data.balances.length,
+    },
+  })
+  if (data === 0) {
+    return (
+      <div className="bg-blue w-full pl-4 text-sm text-secondary">
+        Welcome to Renegade! Deposit your Arbitrum tokens to get started
+        trading.
+        <TransferDialog>
+          <Button className="text-secondary" variant="link">
+            Deposit now
+            <ArrowRight className="ml-1 h-4 w-4" />
+          </Button>
+        </TransferDialog>
+      </div>
+    )
+  }
+  return null
+}
