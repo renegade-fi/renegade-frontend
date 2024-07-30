ed29266846663b4ee5ac5d63c1593d5d7c4753d5
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-29 18:59:27 -0700

    format

--- hooks/use-refresh-on-block.ts
+++ hooks/use-refresh-on-block.ts
@@ -1,21 +1,17 @@
-import { QueryKey, useQueryClient } from "@tanstack/react-query";
-import React from "react";
-import { useBlockNumber } from "wagmi";
+import React from "react"
+
+import { QueryKey, useQueryClient } from "@tanstack/react-query"
+import { useBlockNumber } from "wagmi"
 
 const N = BigInt(2)
 
-export function useRefreshOnBlock({
-    queryKey,
-}: {
-    queryKey: QueryKey
-}) {
-    const queryClient = useQueryClient()
-    const { data } = useBlockNumber({ watch: true })
+export function useRefreshOnBlock({ queryKey }: { queryKey: QueryKey }) {
+  const queryClient = useQueryClient()
+  const { data } = useBlockNumber({ watch: true })
 
-    React.useEffect(() => {
-        if (data && data % N === BigInt(0)) {
-            queryClient.invalidateQueries({ queryKey })
-        }
-    }, [data, queryClient, queryKey])
-
+  React.useEffect(() => {
+    if (data && data % N === BigInt(0)) {
+      queryClient.invalidateQueries({ queryKey })
+    }
+  }, [data, queryClient, queryKey])
 }
