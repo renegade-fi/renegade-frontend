c7629e3ea881f34296a6a41759e882b9cfb4ea9e
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-31 13:32:13 -0700

    check chain before depositing

---
+++ hooks/use-check-chain.ts
@@ -0,0 +1,20 @@
+
+import { useChainId, useConnections, useSwitchChain } from "wagmi"
+
+export function useCheckChain() {
+    const chainId = useChainId()
+    const connections = useConnections()
+    const { switchChainAsync } = useSwitchChain()
+    const handleCheckChain = async () => {
+        if (!connections[0]?.chainId) {
+            // TODO: Open connect wallet modal
+            return
+        }
+        if (chainId !== connections[0].chainId) {
+            await switchChainAsync({
+                chainId,
+            })
+        }
+    }
+    return { checkChain: handleCheckChain }
+}
