395a4d2c27340f95369057469be79a68bdabb635
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-31 10:46:15 -0700

    format

--- lib/task.ts
+++ lib/task.ts
@@ -1,38 +1,38 @@
-import { Task, TaskType, UpdateType } from "@renegade-fi/react";
+import { Task, TaskType, UpdateType } from "@renegade-fi/react"
 
 export function isWithdrawTask(task: Task): task is Task & {
-    task_info: {
-        task_type: TaskType.UpdateWallet;
-        update_type: UpdateType.Withdraw;
-        mint: `0x${string}`;
-        amount: bigint;
-    }
+  task_info: {
+    task_type: TaskType.UpdateWallet
+    update_type: UpdateType.Withdraw
+    mint: `0x${string}`
+    amount: bigint
+  }
 } {
-    return (
-        task.task_info.task_type === TaskType.UpdateWallet &&
-        task.task_info.update_type === UpdateType.Withdraw
-    );
+  return (
+    task.task_info.task_type === TaskType.UpdateWallet &&
+    task.task_info.update_type === UpdateType.Withdraw
+  )
 }
 
 export function isPayFeesTask(task: Task): task is Task & {
-    task_info: {
-        task_type: TaskType.PayOfflineFee;
-        mint: `0x${string}`;
-        amount: bigint;
-        is_protocol: boolean;
-    }
+  task_info: {
+    task_type: TaskType.PayOfflineFee
+    mint: `0x${string}`
+    amount: bigint
+    is_protocol: boolean
+  }
 } {
-    return task.task_info.task_type === TaskType.PayOfflineFee;
+  return task.task_info.task_type === TaskType.PayOfflineFee
 }
 
 export function isSettleMatchTask(task: Task): task is Task & {
-    task_info: {
-        task_type: TaskType.SettleMatch;
-        base: `0x${string}`;
-        is_sell: boolean;
-        quote: `0x${string}`;
-        volume: bigint;
-    }
+  task_info: {
+    task_type: TaskType.SettleMatch
+    base: `0x${string}`
+    is_sell: boolean
+    quote: `0x${string}`
+    volume: bigint
+  }
 } {
-    return task.task_info.task_type === TaskType.SettleMatch;
+  return task.task_info.task_type === TaskType.SettleMatch
 }
