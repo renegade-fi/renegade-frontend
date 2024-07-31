8c3a8caf2428a859718309914fc2b0f66867ef02
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-31 13:18:41 -0700

    add hook to check if max balances reached

---
+++ components/dialogs/transfer/use-is-max-balances.ts
@@ -0,0 +1,25 @@
+import { useWallet } from "@renegade-fi/react"
+import { MAX_BALANCES } from "@renegade-fi/react/constants"
+
+export function useIsMaxBalances(mint: string) {
+    const { data } = useWallet({
+        query: {
+            select: data =>
+                data.balances.filter(balance =>
+                    Boolean(
+                        balance.amount ||
+                        balance.protocol_fee_balance ||
+                        balance.relayer_fee_balance,
+                    ),
+                ),
+        },
+    })
+
+    let isMaxBalances = false
+    if (mint) {
+        isMaxBalances =
+            data?.length === MAX_BALANCES && !data?.some(bal => bal.mint === mint)
+    }
+
+    return isMaxBalances
+}
