1f1dc00fa872801ce7f97ef8c0b4b4c2180d99b7
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-07-31 13:33:09 -0700

    format

--- hooks/usePrepareCancelOrder.ts
+++ hooks/usePrepareCancelOrder.ts
@@ -1,29 +1,32 @@
 "use client"
 
-import { stringifyForWasm, useBackOfQueueWallet, useConfig } from "@renegade-fi/react"
-import { CancelOrderParameters } from "@renegade-fi/react/actions"
 import React from "react"
+
+import {
+  stringifyForWasm,
+  useBackOfQueueWallet,
+  useConfig,
+} from "@renegade-fi/react"
+import { CancelOrderParameters } from "@renegade-fi/react/actions"
 
 export type UsePrepareCancelOrderParameters = CancelOrderParameters
 
-
 export type UsePrepareCancelOrderReturnType = {
-    request: string | undefined
+  request: string | undefined
 }
 
-export function usePrepareCancelOrder(parameters: UsePrepareCancelOrderParameters) {
-    const { id } = parameters
-    const config = useConfig()
-    const { data: wallet, isSuccess } = useBackOfQueueWallet()
-    const request = React.useMemo(() => {
-        if (!isSuccess) return ""
-        if (wallet.orders.find(order => order.id === id)) {
-            return config.utils.cancel_order(
-                stringifyForWasm(wallet),
-                id,
-            )
-        }
-        return undefined
-    }, [config, wallet, id, isSuccess])
-    return { request }
+export function usePrepareCancelOrder(
+  parameters: UsePrepareCancelOrderParameters,
+) {
+  const { id } = parameters
+  const config = useConfig()
+  const { data: wallet, isSuccess } = useBackOfQueueWallet()
+  const request = React.useMemo(() => {
+    if (!isSuccess) return ""
+    if (wallet.orders.find(order => order.id === id)) {
+      return config.utils.cancel_order(stringifyForWasm(wallet), id)
+    }
+    return undefined
+  }, [config, wallet, id, isSuccess])
+  return { request }
 }
