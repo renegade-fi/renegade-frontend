320dcba24f6fec06cd82cf7273d5c4993c9fa3f6
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-05 16:15:44 -0700

    add all connectors, style connectkit

--- providers/wagmi-provider/wagmi-provider.tsx
+++ providers/wagmi-provider/wagmi-provider.tsx
@@ -4,19 +4,62 @@ import React from "react"
 
 import { useConfig } from "@renegade-fi/react"
 import { disconnect } from "@renegade-fi/react/actions"
-import { ConnectKitProvider } from "connectkit"
+import { ConnectKitProvider, getDefaultConfig } from "connectkit"
 import {
   WagmiProvider as Provider,
   State,
+  cookieStorage,
+  createConfig,
+  createStorage,
+  http,
   useAccount,
   useAccountEffect,
 } from "wagmi"
 
 import { SignInDialog } from "@/components/dialogs/sign-in-dialog"
 
+import { chain } from "@/lib/viem"
 import { QueryProvider } from "@/providers/query-provider"
 
-import { config } from "./config"
+export const config = createConfig(
+  getDefaultConfig({
+    chains: [chain],
+    transports: {
+      [chain.id]: http(),
+    },
+    ssr: true,
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+
+    appName: "Renegade",
+    appDescription: "On-chain dark pool",
+    appUrl: "https://trade.renegade.fi",
+    appIcon: "/glyph_dark.svg",
+  }),
+)
+
+const connectKitTheme = {
+  "--ck-body-background": "hsl(var(--background))",
+  "--ck-border-radius": "0",
+  "--ck-font-family": "var(--font-sans-extended)",
+  "--ck-primary-button-background": "hsl(var(--background))",
+  "--ck-primary-button-border-radius": "0",
+  "--ck-body-color": "hsl(var(--foreground))",
+  "--ck-body-color-muted": "hsl(var(--muted-foreground))",
+  "--ck-body-color-muted-hover": "hsl(var(--foreground))",
+  "--ck-qr-dot-color": "hsl(var(--chart-blue))",
+  "--ck-secondary-button-background": "hsl(var(--background))",
+  "--ck-qr-border-color": "hsl(var(--border))",
+  "--ck-overlay-background": "rgba(0,0,0,.8)",
+}
+
+const connectKitOptions = {
+  hideQuestionMarkCTA: true,
+  hideTooltips: true,
+}
 
 export function WagmiProvider({
   initialState,
@@ -30,7 +73,12 @@ export function WagmiProvider({
   return (
     <Provider config={config} initialState={initialState}>
       <QueryProvider>
-        <ConnectKitProvider onConnect={onOpenChange}>
+        <ConnectKitProvider
+          customTheme={connectKitTheme}
+          onConnect={onOpenChange}
+          options={connectKitOptions}
+          theme="midnight"
+        >
           {children}
           <SyncRenegadeWagmiState />
         </ConnectKitProvider>
