000f583410eb255ad089a7efbca3580546b7e53d
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-13 18:26:24 -0700

    modify tooltip speed and cursor

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -9,7 +9,17 @@ const TooltipProvider = TooltipPrimitive.Provider
 
 const Tooltip = TooltipPrimitive.Root
 
-const TooltipTrigger = TooltipPrimitive.Trigger
+const TooltipTrigger = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TooltipPrimitive.Trigger
+    ref={ref}
+    className={cn('cursor-default', className)}
+    {...props}
+  />
+))
+TooltipTrigger.displayName = TooltipPrimitive.Trigger.displayName
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
