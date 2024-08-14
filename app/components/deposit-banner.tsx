8a5cf08ae395ed96231a7a5a8449c14a399676eb
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-13 18:11:42 -0700

    change deposit banner color

--- app/components/deposit-banner.tsx
+++ app/components/deposit-banner.tsx
@@ -1,30 +1,44 @@
 "use client"
 
+import { Cross2Icon } from "@radix-ui/react-icons"
 import { useWallet } from "@renegade-fi/react"
 import { ArrowRight } from "lucide-react"
+import { useLocalStorage } from "usehooks-ts"
 
 import { TransferDialog } from "@/components/dialogs/transfer/transfer-dialog"
 import { Button } from "@/components/ui/button"
 
+import { STORAGE_DEPOSIT_BANNER } from "@/lib/constants/storage"
+
 export function DepositBanner() {
+  const [isClosed, setIsClosed] = useLocalStorage(STORAGE_DEPOSIT_BANNER, false)
   const { data } = useWallet({
     query: {
       select: data => data.balances.length,
     },
   })
-  if (data === 0) {
+  if ((data === 0 && !isClosed) || true) {
     return (
-      <div className="w-full bg-blue pl-4 text-sm text-secondary">
-        Welcome to Renegade! Deposit your Arbitrum tokens to get started
-        trading.
+      <div className="flex w-full items-center border-b border-border bg-[#00183e] pl-4 text-sm text-blue">
+        <div>
+          Welcome to Renegade! Deposit your Arbitrum tokens to get started.
+        </div>
         <TransferDialog>
-          <Button className="text-secondary" variant="link">
+          <Button className="text-blue" variant="link">
             Deposit now
             <ArrowRight className="ml-1 h-4 w-4" />
           </Button>
         </TransferDialog>
+        <Button
+          className="ml-auto"
+          size="icon"
+          variant="ghost"
+          onClick={() => setIsClosed(true)}
+        >
+          <Cross2Icon className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </Button>
       </div>
     )
   }
-  return null
 }
--- lib/constants/storage.ts
+++ lib/constants/storage.ts
@@ -6,3 +6,5 @@ export const STORAGE_LAYOUT = `${STORAGE_PREFIX}.layout`
 export const STORAGE_AMOUNT = `${STORAGE_PREFIX}.order.amount`
 export const STORAGE_IS_USDC_DENOMINATED = `${STORAGE_PREFIX}.order.isUSDCDenominated`
 export const STORAGE_BASE = `${STORAGE_PREFIX}.base`
+
+export const STORAGE_DEPOSIT_BANNER = `${STORAGE_PREFIX}.deposit-banner`
