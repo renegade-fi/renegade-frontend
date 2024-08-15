d7ccddefe814f84f94945bb588b8ea70379337d1
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-15 13:40:12 -0700

    history table: add decimal switch

--- app/assets/history-table/columns.tsx
+++ app/assets/history-table/columns.tsx
@@ -1,13 +1,11 @@
 import { Token } from "@renegade-fi/react"
 import { ColumnDef } from "@tanstack/react-table"
-import { ChevronDown, ChevronUp, ChevronsUpDown } from "lucide-react"
 
 import { HistoryData } from "@/app/assets/page-client"
 
 import { TokenIcon } from "@/components/token-icon"
-import { Button } from "@/components/ui/button"
 
-import { formatTimestamp } from "@/lib/format"
+import { formatNumber, formatTimestamp } from "@/lib/format"
 
 export const columns: ColumnDef<HistoryData>[] = [
   {
@@ -26,10 +24,7 @@ export const columns: ColumnDef<HistoryData>[] = [
       const token = Token.findByAddress(mint)
       return (
         <div className="flex items-center gap-2 font-medium">
-          <TokenIcon
-            size={20}
-            ticker={token.ticker}
-          />
+          <TokenIcon size={20} ticker={token.ticker} />
           {token.ticker}
         </div>
       )
@@ -47,9 +42,18 @@ export const columns: ColumnDef<HistoryData>[] = [
   {
     accessorKey: "amount",
     header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = row.getValue<bigint>("amount")
-      return <div className="text-right">{amount}</div>
+    cell: ({ row, table }) => {
+      const amount = row.original.rawAmount
+      const token = Token.findByAddress(row.original.mint)
+      return (
+        <div className="text-right">
+          {formatNumber(
+            amount,
+            token.decimals,
+            table.options.meta?.isLongFormat,
+          )}
+        </div>
+      )
     },
   },
   {
