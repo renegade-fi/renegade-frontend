44b6ded0dd4f13d387d7e3a9979c26bc89c5db26
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-16 15:03:41 -0700

    data table: row cursor pointer

--- app/orders/data-table.tsx
+++ app/orders/data-table.tsx
@@ -222,6 +222,7 @@ export function DataTable<TData, TValue>({
                     <TableRow
                       key={row.id}
                       data-state={row.getIsSelected() && "selected"}
+                      className="cursor-pointer"
                     >
                       {row.getVisibleCells().map((cell) => (
                         <TableCell key={cell.id}>
