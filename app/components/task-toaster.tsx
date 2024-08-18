a332844e2670600f69388da5970545098c254d7b
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-17 18:55:27 -0700

    add cancell all orders button

--- app/components/task-toaster.tsx
+++ app/components/task-toaster.tsx
@@ -13,6 +13,7 @@ import {
   generateStartToastMessage,
 } from "@/lib/constants/task"
 import {
+  isCancelOrderTask,
   isDepositTask,
   isPayFeesTask,
   isSettleMatchTask,
@@ -20,106 +21,114 @@ import {
 } from "@/lib/task"
 
 export function TaskToaster() {
-  const [incomingTask, setIncomingTask] = React.useState<Task>()
-  const taskIdToStateMap = React.useRef<Map<string, Task>>(new Map())
+  const seen = React.useRef<Map<string, Task>>(new Map())
   useTaskHistoryWebSocket({
     onUpdate(task) {
-      if (taskIdToStateMap.current.get(task.id)?.state === task.state) {
+      if (seen.current.get(task.id)?.state === task.state) {
         return
       }
-      taskIdToStateMap.current.set(task.id, task)
-      setIncomingTask(task)
+      seen.current.set(task.id, task)
+      if (isWithdrawTask(task)) {
+        processWithdrawTask(task)
+      } else {
+        processTask(task)
+      }
     },
   })
 
-  React.useEffect(() => {
-    if (incomingTask) {
-      // Order toaster handles SettleMatch task completion
-      // Ignore pay fees tasks
-      // Handle withdraw tasks separately
-      if (
-        isSettleMatchTask(incomingTask) ||
-        isPayFeesTask(incomingTask) ||
-        isWithdrawTask(incomingTask)
-      ) {
-        return
-      }
-
-      const state = formatTaskState(incomingTask.state)
-      console.log("state in effect 1", state)
-
-      if (incomingTask.state === "Completed") {
-        const message = generateCompletionToastMessage(incomingTask)
-        toast.success(message, {
-          id: incomingTask.id,
-          description: state,
-        })
-        return
-      } else if (incomingTask.state === "Failed") {
-        const message = generateFailedToastMessage(incomingTask)
-        toast.error(message, {
-          id: incomingTask.id,
-        })
-      } else if (incomingTask.state === "Proving") {
-        const message = generateStartToastMessage(incomingTask)
-        toast.loading(message, {
-          id: incomingTask.id,
-          description: state,
-        })
-      } else if (
-        incomingTask.state === "Updating Validity Proofs" &&
-        isDepositTask(incomingTask)
-      ) {
-        const message = generateCompletionToastMessage(incomingTask)
-        toast.success(message, {
-          id: incomingTask.id,
-          description: "Completed",
-        })
-      } else {
-        const message = generateStartToastMessage(incomingTask)
-        toast.loading(message, {
-          id: incomingTask.id,
-          description: state,
-        })
-      }
-    }
-  }, [incomingTask])
-
-  React.useEffect(() => {
-    if (incomingTask && isWithdrawTask(incomingTask)) {
-      const state = formatTaskState(incomingTask.state)
-      const id = WITHDRAW_TOAST_ID(
-        incomingTask.task_info.mint,
-        incomingTask.task_info.amount,
-      )
-      if (incomingTask.state === "Completed") {
-        const message = generateCompletionToastMessage(incomingTask)
-        toast.success(message, {
-          id,
-          description: state,
-        })
-        return
-      } else if (incomingTask.state === "Failed") {
-        const message = generateFailedToastMessage(incomingTask)
-        toast.error(message, {
-          id,
-        })
-      } else if (incomingTask.state === "Updating Validity Proofs") {
-        const message = generateCompletionToastMessage(incomingTask)
-        toast.success(message, {
-          id,
-          description: "Completed",
-        })
-      } else if (incomingTask.state === "Proving") {
-        return
-      } else {
-        const message = generateStartToastMessage(incomingTask)
-        toast.loading(message, {
-          id,
-          description: state,
-        })
-      }
-    }
-  }, [incomingTask])
   return null
+}
+
+function processTask(incomingTask: Task) {
+  // Order toaster handles SettleMatch task completion
+  // Ignore pay fees tasks
+  // Handle withdraw tasks separately
+  if (
+    isSettleMatchTask(incomingTask) ||
+    isPayFeesTask(incomingTask) ||
+    isWithdrawTask(incomingTask)
+  ) {
+    return
+  }
+
+  const state = formatTaskState(incomingTask.state)
+  if (isCancelOrderTask(incomingTask)) {
+    console.log("debug incoming task", {
+      id: incomingTask.id,
+      n: incomingTask.task_info.amount,
+      state,
+    })
+  }
+
+  if (incomingTask.state === "Completed") {
+    const message = generateCompletionToastMessage(incomingTask)
+    toast.success(message, {
+      id: incomingTask.id,
+      description: state,
+    })
+    return
+  } else if (incomingTask.state === "Failed") {
+    const message = generateFailedToastMessage(incomingTask)
+    toast.error(message, {
+      id: incomingTask.id,
+    })
+  } else if (incomingTask.state === "Proving") {
+    const message = generateStartToastMessage(incomingTask)
+    toast.loading(message, {
+      id: incomingTask.id,
+      description: state,
+    })
+  } else if (
+    incomingTask.state === "Updating Validity Proofs" &&
+    isDepositTask(incomingTask)
+  ) {
+    const message = generateCompletionToastMessage(incomingTask)
+    toast.success(message, {
+      id: incomingTask.id,
+      description: "Completed",
+    })
+  } else {
+    const message = generateStartToastMessage(incomingTask)
+    toast.loading(message, {
+      id: incomingTask.id,
+      description: state,
+    })
+  }
+}
+
+function processWithdrawTask(incomingTask: Task) {
+  if (isWithdrawTask(incomingTask)) {
+    const state = formatTaskState(incomingTask.state)
+    const id = WITHDRAW_TOAST_ID(
+      incomingTask.task_info.mint,
+      incomingTask.task_info.amount,
+    )
+    if (incomingTask.state === "Completed") {
+      const message = generateCompletionToastMessage(incomingTask)
+      toast.success(message, {
+        id,
+        description: state,
+      })
+      return
+    } else if (incomingTask.state === "Failed") {
+      const message = generateFailedToastMessage(incomingTask)
+      toast.error(message, {
+        id,
+      })
+    } else if (incomingTask.state === "Updating Validity Proofs") {
+      const message = generateCompletionToastMessage(incomingTask)
+      toast.success(message, {
+        id,
+        description: "Completed",
+      })
+    } else if (incomingTask.state === "Proving") {
+      return
+    } else {
+      const message = generateStartToastMessage(incomingTask)
+      toast.loading(message, {
+        id,
+        description: state,
+      })
+    }
+  }
 }
--- lib/task.ts
+++ lib/task.ts
@@ -1,5 +1,17 @@
 import { Task, TaskType, UpdateType } from "@renegade-fi/react"
 
+export function isCancelOrderTask(task: Task): task is Task & {
+  task_info: {
+    task_type: TaskType.UpdateWallet
+    update_type: UpdateType.CancelOrder
+  }
+} {
+  return (
+    task.task_info.task_type === TaskType.UpdateWallet &&
+    task.task_info.update_type === UpdateType.CancelOrder
+  )
+}
+
 export function isDepositTask(task: Task): task is Task & {
   task_info: {
     task_type: TaskType.UpdateWallet
