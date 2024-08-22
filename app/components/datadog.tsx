ff2a68104b6e54d4e67e08fe05271ad0064b90c8
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-22 12:42:28 -0700

    app: add datadog

---
+++ app/components/datadog.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import React from "react"
+
+export function LazyDatadog() {
+  React.useEffect(() => {
+    if (
+      process.env.NODE_ENV === "development" ||
+      process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+    ) {
+      return
+    }
+
+    let datadogRum: any
+    let datadogLogs
+
+    async function loadLogging() {
+      const datadogRumModule = await import("@datadog/browser-rum")
+      const datadogLogsModule = await import("@datadog/browser-logs")
+
+      datadogRum = datadogRumModule.datadogRum
+      datadogLogs = datadogLogsModule.datadogLogs
+
+      datadogRum.init({
+        applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID,
+        clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
+        site: "us5.datadoghq.com",
+        service: "testnet-interface",
+        env: "testnet",
+        version: "1.0.0",
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 100,
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        defaultPrivacyLevel: "allow",
+        startSessionReplayRecordingManually: true,
+      })
+
+      datadogLogs.init({
+        clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
+        site: "us5.datadoghq.com",
+        service: "testnet-interface",
+        env: "testnet",
+        forwardErrorsToLogs: true,
+        forwardConsoleLogs: "all",
+        sessionSampleRate: 100,
+      })
+
+      datadogRum.startSessionReplayRecording()
+    }
+
+    loadLogging()
+
+    return () => {
+      if (datadogRum) {
+        datadogRum.stopSessionReplayRecording()
+      }
+    }
+  }, [])
+
+  return null
+}
