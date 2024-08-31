b4e65f1415fbb5f9369685c6e3609f6e0b55dde4
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-08-31 14:38:35 -0700

    order details sheet: refactor and improve empty state

---
+++ app/trade/[base]/components/order-details/empty-content.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image"
+
+import { OrderMetadata, OrderState, Token } from "@renegade-fi/react"
+import { formatUnits } from "viem/utils"
+
+import { CancelButton } from "@/app/trade/[base]/components/order-details/cancel-button"
+import { InsufficientWarning } from "@/app/trade/[base]/components/order-details/insufficient-warning"
+import { OrderStatusIndicator } from "@/app/trade/[base]/components/order-details/order-status-indicator"
+
+import { Separator } from "@/components/ui/separator"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+import { Side } from "@/lib/constants/protocol"
+import { formatNumber, formatOrderState, formatPercentage } from "@/lib/format"
+
+export function EmptyContent({ order }: { order: OrderMetadata }) {
+  const token = Token.findByAddress(order.data.base_mint)
+
+  const filledAmount = order.fills.reduce(
+    (acc, fill) => acc + fill.amount,
+    BigInt(0),
+  )
+  const formattedFilledAmount = formatNumber(filledAmount, token.decimals)
+  const formattedFilledAmountLong = formatUnits(filledAmount, token.decimals)
+  const percentageFilled =
+    (Number(filledAmount) / Number(order.data.amount)) * 100
+  const percentageFilledLabel = formatPercentage(
+    Number(filledAmount),
+    Number(order.data.amount),
+  )
+
+  const formattedTotalAmount = formatNumber(
+    order.data.amount,
+    token.decimals,
+    true,
+  )
+  const formattedTotalAmountLong = formatUnits(
+    order.data.amount,
+    token.decimals,
+  )
+  const title = `${order.data.side === "Buy" ? "Buy" : "Sell"} ${formattedTotalAmount} ${token.ticker} ${
+    order.data.side === "Buy" ? "with" : "for"
+  } USDC`
+  const titleLong = `${order.data.side === "Buy" ? "Buy" : "Sell"} ${formattedTotalAmountLong} ${token.ticker} ${
+    order.data.side === "Buy" ? "with" : "for"
+  } USDC`
+
+  const isCancellable = [OrderState.Created, OrderState.Matching].includes(
+    order.state,
+  )
+  const isOpen =
+    order.state !== OrderState.Filled && order.state !== OrderState.Cancelled
+
+  return (
+    <>
+      <div className="flex p-6">
+        <OrderStatusIndicator order={order} />
+        {isOpen && (
+          <InsufficientWarning
+            className="text-sm font-bold"
+            amount={order.data.amount}
+            baseMint={order.data.base_mint}
+            quoteMint={order.data.quote_mint}
+            side={order.data.side === "Buy" ? Side.BUY : Side.SELL}
+          />
+        )}
+        <div className="ml-auto flex">
+          <CancelButton
+            id={order.id}
+            isDisabled={!isCancellable}
+          />
+        </div>
+      </div>
+      <Separator />
+      <div className="flex h-24 items-center">
+        <div className="flex-1 px-6">
+          <div className="text-sm">{formatOrderState(order.state)}</div>
+          <Tooltip>
+            <TooltipTrigger>
+              <div className="text-sm">{title}</div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="font-sans">{titleLong}</p>
+            </TooltipContent>
+          </Tooltip>
+          <div className="text-sm">Midpoint Peg</div>
+        </div>
+      </div>
+      <Separator />
+      <div className="grid h-[500px] place-items-center">
+        <div className="space-y-10">
+          <Image
+            className="mx-auto animate-pulse"
+            src="/glyph_dark.svg"
+            alt="logo"
+            width="46"
+            height="57"
+            priority
+          />
+          <div className="text-sm text-muted-foreground">
+            Once your order is filled, you&apos;ll see the details here.
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
