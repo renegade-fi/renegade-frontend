542ee1c086e7b4c77c97f4e99e7917bf993a32c8
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-09-02 18:33:34 -0700

    viem: set chain to either mainnet or testnet

--- lib/viem.ts
+++ lib/viem.ts
@@ -1,28 +1,7 @@
 import { createPublicClient, defineChain, http } from "viem"
-import { arbitrumSepolia } from "viem/chains"
-
-const renegadeDevnet = defineChain({
-  id: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-  name: "Renegade Devnet",
-  testnet: true,
-  nativeCurrency: {
-    decimals: 18,
-    name: "Ether",
-    symbol: "ETH",
-  },
-  rpcUrls: {
-    default: {
-      http: [process.env.NEXT_PUBLIC_RPC_URL],
-    },
-  },
-})
-
-export const chain =
-  process.env.NEXT_PUBLIC_CHAIN_ID === "421614"
-    ? arbitrumSepolia
-    : renegadeDevnet
+import { arbitrumSepolia, arbitrum } from "viem/chains"
 
 export const viemClient = createPublicClient({
-  chain,
+  chain: Number(process.env.NEXT_PUBLIC_CHAIN_ID) === arbitrum.id ? arbitrum : arbitrumSepolia,
   transport: http(process.env.NEXT_PUBLIC_RPC_URL),
 })
