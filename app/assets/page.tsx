7228f6795d5d4db63b2933399525dda103321475
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-09-03 22:51:54 -0700

    assets: disable prefetching for perf

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -4,19 +4,16 @@ import {
   dehydrate,
 } from "@tanstack/react-query"
 
-import { prefetchPrice } from "@/app/actions"
 import { PageClient } from "@/app/assets/page-client"
 import { Footer } from "@/app/components/footer"
 import { Header } from "@/app/components/header"
 
-import { DISPLAY_TOKENS } from "@/lib/token"
-
 export default async function Page() {
   const queryClient = new QueryClient()
 
-  await Promise.all(
-    DISPLAY_TOKENS().map((token) => prefetchPrice(queryClient, token.address)),
-  )
+  // await Promise.all(
+  //   DISPLAY_TOKENS().map((token) => prefetchPrice(queryClient, token.address)),
+  // )
 
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
