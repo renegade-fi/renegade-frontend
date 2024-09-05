b6c10327dd0676abbade7c61f5bb605fdd86155e
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-09-05 15:20:52 -0700

    transfer dialog: alert user if transfer amount is <1 USDC

--- components/dialogs/transfer/transfer-dialog.tsx
+++ components/dialogs/transfer/transfer-dialog.tsx
@@ -335,6 +335,18 @@ function TransferForm({
   usePriceQuery(baseToken?.address || "0x")
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
+    const isAmountSufficient = checkAmount(
+      queryClient,
+      values.amount,
+      baseToken,
+    )
+    if (!isAmountSufficient) {
+      form.setError("amount", {
+        message: `Amount must be greater than or equal to ${MIN_DEPOSIT_AMOUNT} USDC`,
+      })
+      return
+    }
+
     if (direction === ExternalTransferDirection.Deposit) {
       await checkChain()
       const isBalanceSufficient = checkBalance({
@@ -345,17 +357,6 @@ function TransferForm({
       if (!isBalanceSufficient) {
         form.setError("amount", {
           message: "Insufficient Arbitrum balance",
-        })
-        return
-      }
-      const isAmountSufficient = checkAmount(
-        queryClient,
-        values.amount,
-        baseToken,
-      )
-      if (!isAmountSufficient) {
-        form.setError("amount", {
-          message: `Amount must be greater than or equal to ${MIN_DEPOSIT_AMOUNT} USDC`,
         })
         return
       }
