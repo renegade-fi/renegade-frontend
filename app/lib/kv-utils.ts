43d5f4d7ea97873629890d84e4a401c02e971ea6
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-09-09 11:22:44 -0700

    stats: fix fetch set members helper

--- app/lib/kv-utils.ts
+++ app/lib/kv-utils.ts
@@ -2,21 +2,8 @@ import { VercelKV } from "@vercel/kv"
 
 export async function getAllSetMembers(
   kv: VercelKV,
-  key: string,
-  batchSize: number = 100,
+  key: string
 ): Promise<string[]> {
-  let cursor = 0
-  const allMembers: string[] = []
-  do {
-    const [nextCursor, members] = await kv.sscan(key, cursor.toString(), {
-      count: batchSize,
-    })
-    cursor = typeof nextCursor === "string" ? parseInt(nextCursor) : nextCursor
-    allMembers.push(
-      ...members.filter(
-        (member): member is string => typeof member === "string",
-      ),
-    )
-  } while (cursor !== 0)
-  return allMembers
+  const members = await kv.smembers(key)
+  return members.filter((member): member is string => typeof member === "string")
 }
