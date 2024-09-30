040f10618435b7437de9c5fb39efdcbacd07c138
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-09-30 11:27:59 -0700

    stats: unset fetchCache + usntable_noStore

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,13 +1,8 @@
-import { unstable_noStore } from "next/cache"
-
 import { Footer } from "@/app/components/footer"
 import { Header } from "@/app/components/header"
 import { PageClient } from "@/app/stats/page-client"
 
-export const fetchCache = "force-no-store"
-
 export default function Page() {
-  unstable_noStore()
   return (
     <div className="grid min-h-screen grid-cols-1 grid-rows-[auto_1fr_auto_auto]">
       <div className="min-h-20">
