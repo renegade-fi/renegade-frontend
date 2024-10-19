2135101adab6fc8570954433b70316a5e8f10ef0
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-10-19 09:25:49 -0700

    transfer dialog: refactor usdc form

--- components/dialogs/transfer/hooks/use-chain-balance.tsx
+++ components/dialogs/transfer/hooks/use-chain-balance.tsx
@@ -1,8 +1,7 @@
+import { Token } from "@renegade-fi/react"
 import { mainnet } from "viem/chains"
 import { formatUnits } from "viem/utils"
 import { useAccount } from "wagmi"
-
-import { useToken } from "@/components/dialogs/transfer/hooks/use-token"
 
 import { formatNumber } from "@/lib/format"
 import { useReadErc20BalanceOf } from "@/lib/generated"
@@ -11,30 +10,29 @@ import { mainnetConfig } from "@/providers/wagmi-provider/wagmi-provider"
 
 export function useChainBalance({
   chainId = chain.id,
-  mint,
+  token,
   enabled = true,
 }: {
   chainId?: number
-  mint: string
+  token?: Token
   enabled?: boolean
 }) {
   const { address } = useAccount()
-  const baseToken = useToken({ chainId, mint })
   const { data: balance, queryKey } = useReadErc20BalanceOf({
-    address: baseToken?.address,
+    address: token?.address,
     args: [address ?? "0x"],
     config: chainId === mainnet.id ? mainnetConfig : undefined,
     query: {
-      enabled: enabled && !!baseToken && !!address,
+      enabled: enabled && !!token && !!address,
       staleTime: 0,
     },
   })
 
-  const formattedBalance = baseToken
-    ? formatUnits(balance ?? BigInt(0), baseToken.decimals)
+  const formattedBalance = token
+    ? formatUnits(balance ?? BigInt(0), token.decimals)
     : ""
-  const balanceLabel = baseToken
-    ? formatNumber(balance ?? BigInt(0), baseToken.decimals, true)
+  const balanceLabel = token
+    ? formatNumber(balance ?? BigInt(0), token.decimals, true)
     : ""
 
   return {
@@ -42,5 +40,6 @@ export function useChainBalance({
     string: formattedBalance,
     formatted: balanceLabel,
     nonZero: balance && balance !== BigInt(0),
+    queryKey,
   }
 }
