54f093edf44f44421337ef756dcfcc0e399e46ed
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-11-07 12:45:02 -0800

    chore: bump next 15

--- app/api/delete-cookie/route.ts
+++ app/api/delete-cookie/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(request: NextRequest) {
   const cookieName = await request.text()
-  cookies().delete(cookieName)
+  const cookieStore = await cookies()
+  cookieStore.delete(cookieName)
   return new NextResponse(null, { status: 204 })
 }
