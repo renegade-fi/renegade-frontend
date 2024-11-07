99bc6dfc1ab7f7bdde626e6cf2ad8a27ccc42d3e
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-11-07 14:19:13 -0800

    chore: clean up use client directive

--- app/components/clear-cookie.tsx
+++ app/components/clear-cookie.tsx
@@ -10,19 +10,15 @@ export function ClearCookie() {
   React.useEffect(() => {
     const handleBeforeUnload = () => {
       const rememberMe = localStorage.getItem(STORAGE_REMEMBER_ME)
-
       if (rememberMe !== "true") {
         document.cookie = `${STORAGE_STORE}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
         deleteCookie(STORAGE_STORE)
       }
     }
-
     window.addEventListener("beforeunload", handleBeforeUnload)
-
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload)
     }
   }, [])
-
   return null
 }
--- app/components/wallet-sidebar/wallet-button.tsx
+++ app/components/wallet-sidebar/wallet-button.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { ReactNode } from "react"
 
 import { ChevronsUpDown } from "lucide-react"
