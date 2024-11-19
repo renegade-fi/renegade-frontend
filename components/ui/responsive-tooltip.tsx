170c86bb176d3dfe04cba9f48419d386df1c28a5
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-11-19 15:31:01 -0800

    chore: fix hydration issue

--- components/ui/responsive-tooltip.tsx
+++ components/ui/responsive-tooltip.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import React from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
-import { useSharedMediaQuery } from '@/hooks/use-shared-media-query';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { cn } from '@/lib/utils';
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import React from 'react';
 
 type PopoverContentProps = React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>;
 type TooltipContentProps = React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>;
@@ -25,22 +25,19 @@ type ResponsiveTooltipContentProps = TooltipContentProps & PopoverContentProps &
 };
 
 export function ResponsiveTooltipTrigger({ children, ...props }: ResponsiveTooltipTriggerProps) {
-  const isDesktop = useSharedMediaQuery("(min-width: 1024px)");
+  const isMobile = useIsMobile()
 
-  if (isDesktop) {
-    return <TooltipTrigger type="button" {...props}>{children}</TooltipTrigger>;
+  if (isMobile) {
+  return <PopoverTrigger type="button" {...props}>{children}</PopoverTrigger>;
   }
 
-  return <PopoverTrigger type="button" {...props}>{children}</PopoverTrigger>;
+    return <TooltipTrigger type="button" {...props}>{children}</TooltipTrigger>;
 }
 
 export function ResponsiveTooltipContent({ children, side, className, ...props }: ResponsiveTooltipContentProps) {
-  const isDesktop = useSharedMediaQuery("(min-width: 1024px)");
+  const isMobile = useIsMobile()
 
-  if (isDesktop) {
-    return <TooltipContent className={className} side={side} {...props}>{children}</TooltipContent>;
-  }
-
+  if (isMobile) {
   return <PopoverContent
     className={cn(
       'z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
@@ -53,14 +50,17 @@ export function ResponsiveTooltipContent({ children, side, className, ...props }
   >
     {children}
   </PopoverContent>;
+  }
+
+    return <TooltipContent className={className} side={side} {...props}>{children}</TooltipContent>;
 }
 
 export function ResponsiveTooltip({ children, ...props }: ResponsiveTooltipProps) {
-  const isDesktop = useSharedMediaQuery("(min-width: 1024px)");
+  const isMobile = useIsMobile()
 
-  if (isDesktop) {
-    return <Tooltip {...props}>{children}</Tooltip>;
+  if (isMobile) {
+  return <Popover {...props}>{children}</Popover>;
   }
 
-  return <Popover {...props}>{children}</Popover>;
+    return <Tooltip {...props}>{children}</Tooltip>;
 }
