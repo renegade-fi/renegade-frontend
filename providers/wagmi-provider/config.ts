da194ab8616e19c2008904b13f56a520e2fec97b
sehyunc <41171808+sehyunc@users.noreply.github.com>
2024-12-02 14:35:35 -0800

    wagmi: remove connectkit default config

--- providers/wagmi-provider/config.ts
+++ providers/wagmi-provider/config.ts
@@ -1,31 +1,21 @@
-import { getDefaultConfig } from "connectkit"
 import { http } from "viem"
-import { mainnet, arbitrum, arbitrumSepolia } from "viem/chains"
-import { createConfig, createStorage, cookieStorage } from "wagmi"
+import { arbitrum, arbitrumSepolia, mainnet } from "viem/chains"
+import { cookieStorage, createConfig, createStorage } from "wagmi"
 
-import { getURL } from "@/lib/utils"
 import { chain } from "@/lib/viem"
 
-export const wagmiConfig = createConfig(
-  getDefaultConfig({
-    chains: [chain, mainnet],
-    transports: {
-      [chain.id]: http(),
-      [mainnet.id]: http("/api/proxy/mainnet"),
-    },
-    ssr: true,
-    storage: createStorage({
-      storage: cookieStorage,
-    }),
-
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-
-    appName: "Renegade",
-    appDescription: "On-chain dark pool",
-    appUrl: "https://trade.renegade.fi",
-    appIcon: `${getURL()}/glyph_light.svg`,
+export const wagmiConfig = createConfig({
+  chains: [chain, mainnet],
+  transports: {
+    [arbitrum.id]: http(),
+    [arbitrumSepolia.id]: http(),
+    [mainnet.id]: http("/api/proxy/mainnet"),
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
   }),
-)
+})
 
 export const mainnetConfig = createConfig({
   chains: [mainnet],
