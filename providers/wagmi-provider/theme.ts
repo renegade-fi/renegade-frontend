66ddd7cecc31227445d161ba01b0ca3a0039a0d4
sehyunc <41171808+sehyunc@users.noreply.github.com>
2025-05-29 23:45:55 -0700

    state: sync Wagmi state with Renegade state

---
+++ providers/wagmi-provider/theme.ts
@@ -0,0 +1,14 @@
+export const connectKitTheme = {
+  "--ck-body-background": "hsl(var(--background))",
+  "--ck-border-radius": "0",
+  "--ck-font-family": "var(--font-sans-extended)",
+  "--ck-primary-button-background": "hsl(var(--background))",
+  "--ck-primary-button-border-radius": "0",
+  "--ck-body-color": "hsl(var(--foreground))",
+  "--ck-body-color-muted": "hsl(var(--muted-foreground))",
+  "--ck-body-color-muted-hover": "hsl(var(--foreground))",
+  "--ck-qr-dot-color": "hsl(var(--chart-blue))",
+  "--ck-secondary-button-background": "hsl(var(--background))",
+  "--ck-qr-border-color": "hsl(var(--border))",
+  "--ck-overlay-background": "rgba(0,0,0,.8)",
+}
