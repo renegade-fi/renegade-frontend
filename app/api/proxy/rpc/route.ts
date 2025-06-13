46996c9fd375b4a2fd4cdbeb36edeab8749f000c
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2025-06-13 11:17:39 -0700

    rpc: use proxy for read (#328)

--- app/api/proxy/rpc/route.ts
+++ app/api/proxy/rpc/route.ts
@@ -1,18 +1,25 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { mainnet } from "viem/chains"
-
 import { getAlchemyRpcUrl } from "@/app/api/utils"
 
 export const runtime = "edge"
 
-const MAINNET_RPC_URL = getAlchemyRpcUrl(mainnet.id)
-
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
+    const params = req.nextUrl.searchParams
+    const chainId = params.get("id")
+    if (!chainId || Number.isNaN(Number(chainId))) {
+      return NextResponse.json(
+        {
+          error: { code: -32602, message: "Invalid chainId" },
+        },
+        { status: 400 },
+      )
+    }
+    const RPC_URL = getAlchemyRpcUrl(Number(chainId))
     const body = await req.text()
 
-    const response = await fetch(MAINNET_RPC_URL, {
+    const response = await fetch(RPC_URL, {
       method: "POST",
       body: body,
     })
