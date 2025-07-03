24966d186994540d8b1bc7b9a6aae6b0dfbd660c
sehyunc <41171808+sehyunc@users.noreply.github.com>
2025-07-02 19:30:47 -0700

    remove ramp v1

--- app/ramp/sequence-builder/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Sequence builder for constructing ordered transaction steps from user intents.
- */
-
-// Public exports
-export { buildSequence } from "./build-sequence";
--- app/ramp/ui/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * UI components for transaction ramp interface.
- */
-
-// Public exports
-export { IntentForm } from "./intent-form";
-export { TransactionStepper } from "./transaction-stepper";
