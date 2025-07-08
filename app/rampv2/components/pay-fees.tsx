c740298ba0ec67cd061c617f04d27ca2ae3a5b16
sehyunc <41171808+sehyunc@users.noreply.github.com>
2025-07-08 10:48:00 -0700

    remove dead code

--- app/rampv2/components/pay-fees.tsx
+++ app/rampv2/components/pay-fees.tsx
@@ -7,14 +7,8 @@ const MIN_FEES = 2;
 
 /** Effect that pays fees when the number of fees in the back of queue wallet is greater than or equal to MIN_FEES. */
 export function PayFees(props: QueryParams) {
-    const { data: numFees, isSuccess } = useQuery({
-        ...numFeesQuery({
-            renegadeConfig: props.renegadeConfig,
-        }),
-        enabled: !!props.renegadeConfig,
-    });
+    const { data: numFees, isSuccess } = useQuery(numFeesQuery(props));
     useEffect(() => {
-        console.log("num fees", numFees);
         if (!props.renegadeConfig || !isSuccess || numFees < MIN_FEES) return;
         payFees(props.renegadeConfig);
     }, [props.renegadeConfig, isSuccess, numFees]);
